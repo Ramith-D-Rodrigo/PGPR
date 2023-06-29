2638903a3fb9659981eb9b9717d2f2d05b83a1a0
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-06-29 14:15:00 +0530

    started ading attributes

--- server/database/migrations/2023_06_26_161732_create_academic_staff_table.php
+++ server/database/migrations/2023_06_26_161732_create_academic_staff_table.php
@@ -14,6 +14,23 @@ return new class extends Migration
         Schema::create('academic_staff', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table -> string('designation');
+            $table -> json('experience_in_industry');
+            $table -> string('google_scholar_link');
+            $table -> json('nominees'); //nominees from professional bodies (array of objects)
+            $table -> json('department'); //department details of the academic staff (department name, department head name, head email, department postal address)
+            $table -> json('experience_with_research_funds');
+            $table -> integer('supervised_postgraduate_student_count');
+            $table -> integer('publications_in_referred_journals_count');
+            $table -> integer('abstract_count');
+            $table -> integer('conference_preceedings_count');
+            $table -> integer('book_chapters'); //book chapters count
+            $table -> json('involvement_in_internal_quality_assurance');
+            $table -> json('involment_in_study_programme_development');
+            $table -> json('postgraduate_teaching_experience');
+            $table -> json('postgraduate_qualifications'); //postgraduate qualifications (qualiifcation , slqf level of the qualification) [up to 4]
+
+            $table -> string('cv'); //cv file path
 
             //foreign key
             $table -> foreign('id') -> references('id') -> on('university_sides');
