d4ae3274cc29eab25268ad0bf205be06f9aa970f
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-06-30 09:12:48 +0530

    SER foreign keys

--- server/database/migrations/2023_06_29_143711_create_self_evaluation_reports_table.php
+++ server/database/migrations/2023_06_29_143711_create_self_evaluation_reports_table.php
@@ -14,13 +14,16 @@ return new class extends Migration
         Schema::create('self_evaluation_reports', function (Blueprint $table) {
             $table->id();
             $table -> unsignedBigInteger('post_graduate_program_review_id');
-            $table -> unsignedBigInteger('vice_chancellor_id');
-            $table -> unsignedBigInteger('dean_id');
-            $table -> unsignedBigInteger('center_for_quality_assurance_director_id');
+            $table -> unsignedBigInteger('vice_chancellor_id') -> nullable(); //approved by vice chancellor
+            $table -> unsignedBigInteger('dean_id') -> nullable();    //approved by dean
+            $table -> unsignedBigInteger('center_for_quality_assurance_director_id') -> nullable();   //approved by center for quality assurance director
             $table->timestamps();
 
             //foreign keys
             $table -> foreign('post_graduate_program_review_id') -> references('id') -> on('post_graduate_program_review_applications');
+            $table -> foreign('vice_chancellor_id') -> references('id') -> on('vice_chancellors');
+            $table -> foreign('dean_id') -> references('id') -> on('deans');
+            $table -> foreign('center_for_quality_assurance_director_id') -> references('id') -> on('center_for_quality_assurance_directors');
         });
     }
 
