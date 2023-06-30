4bc2f7042a546dc0a29073a6b522a3516bb02b59
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-06-30 09:45:35 +0530

    fix foreign key name too long error

--- server/database/migrations/2023_06_29_143711_create_self_evaluation_reports_table.php
+++ server/database/migrations/2023_06_29_143711_create_self_evaluation_reports_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table -> foreign('post_graduate_program_review_id') -> references('id') -> on('post_graduate_program_review_applications');
             $table -> foreign('vice_chancellor_id') -> references('id') -> on('vice_chancellors');
             $table -> foreign('dean_id') -> references('id') -> on('deans');
-            $table -> foreign('center_for_quality_assurance_director_id') -> references('id') -> on('center_for_quality_assurance_directors');
+            $table -> foreign('center_for_quality_assurance_director_id', 'cqa_dir_id') -> references('id') -> on('center_for_quality_assurance_directors');
         });
     }
 
