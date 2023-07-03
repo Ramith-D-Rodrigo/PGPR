abd9f48530d12f1eedad614eda2b4c0f11caf812
BhathiyaVandebona <malingabhathiya@gmail.com>
2023-07-03 18:32:56 +0530

    added the start_date and the end_date attributes

--- server/database/migrations/2023_06_27_155136_create_desk_evaluations_table.php
+++ server/database/migrations/2023_06_27_155136_create_desk_evaluations_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('desk_evaluations', function (Blueprint $table) {
           $table->id();
           $table->foreignId('pgpr_id');
+          $table->date('start_date');
+          $table->date('end_date');
           $table->timestamps();
 
 
