6c76080a62ed7557951ccad47a2e2199d1326678
BhathiyaVandebona <malingabhathiya@gmail.com>
2023-07-03 18:33:47 +0530

    added the start_date and the end_date attributes + an enum attribute to mark the stage

--- server/database/migrations/2023_06_27_155142_create_proper_evaluations_table.php
+++ server/database/migrations/2023_06_27_155142_create_proper_evaluations_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('proper_evaluations', function (Blueprint $table) {
           $table->id();
           $table->foreignId('pgpr_id');
+          $table->date('start_date');
+          $table->date('end_date');
+          $table->enum("stage", ['1', '2', 'COMPLETED']);
           $table->timestamps();
 
           // indices
