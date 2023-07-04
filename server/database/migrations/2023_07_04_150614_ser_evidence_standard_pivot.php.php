737f68a847f6fbde34fabd44ae340b0f9027eb00
BhathiyaVandebona <malingabhathiya@gmail.com>
2023-07-04 21:09:54 +0530

    added a comment sot the adherence

--- server/database/migrations/2023_07_04_150614_ser_evidence_standard_pivot.php.php
+++ server/database/migrations/2023_07_04_150614_ser_evidence_standard_pivot.php.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('ser_id');
             $table->foreignId('standard_id');
             $table->foreignId('evidence_id');
-            $table->string('adherence');
+            $table->string('adherence'); // to store the url
             $table->timestamps();
 
             $table->index('ser_id');
