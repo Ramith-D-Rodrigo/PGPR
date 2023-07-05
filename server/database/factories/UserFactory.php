b8f5dbd439c9381bbb065f1c06f93eb008ca163d
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-05 09:33:01 +0530

    Update UserFactory.php

--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -18,8 +18,19 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'full_name' => fake()->name(),
+            'initials'=> fake()->randomLetter(),
+            'surname' => fake()->lastName(),
+            'roles' => json_encode(['qac', 'cqa', 'reviewer']),
+            'contact_no' => json_encode(['0712345678', '0776543210']),
+            'profile_pic' => fake()->imageUrl(),
+            'official_telephone_no' => fake()->phoneNumber(),
+            'nic' => strval(fake()->randomNumber(8)),
+            'gender' => fake()->randomElement(['m', 'f']),
+            'official_email' => fake()->unique()->safeEmail(),
+            'personal_email' => fake()->unique()->safeEmail(),
+            'created_by' => 1,
+            'status' => fake()->randomElement(['active', 'inactive']),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
