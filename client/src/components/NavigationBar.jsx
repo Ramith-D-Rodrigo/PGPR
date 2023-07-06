426b9601fe804a4cfff56abd030e8b7399364a65
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-06 22:43:25 +0530

    make navigation box independent

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,23 +1,57 @@
-import {AppBar, Toolbar, Typography} from '@mui/material';
+import {Toolbar, Typography} from '@mui/material';
+import MuiAppBar from '@mui/material/AppBar';
 import {Stack, Avatar} from '@mui/material';
 import BreadCrumbs from './BreadCrumbs';
 import {Divider} from '@mui/material';
+import styled from '@mui/material/styles/styled';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const NavigationBar = () => {
+let drawerWidth = 240; //default value
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+//open and openDrawer are for sidebar (navigation bar is a child of sidebar)
+const NavigationBar = ({open , openDrawer, drawerWidthInput}) => {
+  drawerWidth = drawerWidthInput;
   return (
-    <AppBar position='static' color='transparent' style={{boxShadow: 'none'}}>
-        <Toolbar>
-            <BreadCrumbs/>
-            <Stack direction='row' spacing={2}>
-                
-                <Typography variant='h6' color='black'>
-                    User&apos;s Name
-                </Typography>
-                <Divider orientation='vertical' flexItem color='black'/>
-                <Avatar/>
-            </Stack>
-        </Toolbar>
-    </AppBar>
+      <AppBar color='transparent' style={{boxShadow: 'none'}} open={open} position='absolute'>
+          <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick= {openDrawer}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: 'none' }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <BreadCrumbs/>
+              <Stack direction='row' spacing={2}>
+                  
+                  <Typography variant='h6' color='black'>
+                      User&apos;s Name
+                  </Typography>
+                  <Divider orientation='vertical' flexItem color='black'/>
+                  <Avatar/>
+              </Stack>
+          </Toolbar>
+      </AppBar>
   )
 }
 
