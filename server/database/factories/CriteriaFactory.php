c1e097b093e890b28cfb7c4896b583ad9416dcf9
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-06 13:48:37 +0530

    criteria factory and seeder

--- server/database/factories/CriteriaFactory.php
+++ server/database/factories/CriteriaFactory.php
@@ -20,4 +20,74 @@ class CriteriaFactory extends Factory
             //
         ];
     }
+
+    public function criteria1(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Programme Management',
+                'min_weightage_score' => 75
+            ];
+        });
+    }
+
+    public function criteria2(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Programme Design and Development',
+                'min_weightage_score' => 75
+            ];
+        });
+    }
+
+    public function criteria3(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Human and Physical Resources and Learner Support',
+                'min_weightage_score' => 75
+            ];
+        });
+    }
+
+    public function criteria4(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Teaching- Learning and Research',
+                'min_weightage_score' => 75
+            ];
+        });
+    }
+
+    public function criteria5(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Student Assessment and Award of Qualification',
+                'min_weightage_score' => 75
+            ];
+        });
+    }
+
+    public function criteria6(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Programme Evaluation',
+                'min_weightage_score' => 75
+            ];
+        });
+    }
+
+    public function criteria7(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Innovative and Healthy Practices',
+                'min_weightage_score' => 50
+            ];
+        });
+    }
 }
--- server/database/seeders/CriteriaSeeder.php
+++ server/database/seeders/CriteriaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Criteria;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class CriteriaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //7 criterias
+        Criteria::factory()->criteria1()->create();
+        Criteria::factory()->criteria2()->create();
+        Criteria::factory()->criteria3()->create();
+        Criteria::factory()->criteria4()->create();
+        Criteria::factory()->criteria5()->create();
+        Criteria::factory()->criteria6()->create();
+        Criteria::factory()->criteria7()->create();
+
     }
 }
