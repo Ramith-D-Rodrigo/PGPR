0449b6edd4f15673603c89638e30605942d7df46
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-08 11:44:17 +0530

    change prepareforvalidation

--- server/app/Http/Requests/V1/StoreUniversityRequest.php
+++ server/app/Http/Requests/V1/StoreUniversityRequest.php
@@ -26,15 +26,23 @@ class StoreUniversityRequest extends FormRequest
             "name" => ["required", "string", "max:255", "unique:universities"],
             "address" => ["required", "string", "max:255", "unique:universities"],
             "website" => ["required", "string", "max:255", "unique:universities"],
-            "contact_no" => ["required", "json"], //snake_case is because of the database column name (camelCase is because of the json request field name)
+            "contact_no" => ["required", "json"],
             "fax_no" => ["required", "json"],
         ];
     }
 
     protected function prepareForValidation(){
-        $this->merge([
-            'contact_no' => json_encode($this -> contactNo),
-            'fax_no' => json_encode($this -> faxNo),
-        ]);
+        //convert contactNo and faxNo to json
+        if($this -> has('contactNo') and $this -> contactNo !== 'null'){ //if contactNo is given and not null
+            $this -> merge([
+                'contact_no' => json_encode($this -> contactNo)
+            ]);
+        }
+
+        if($this -> has('faxNo') and $this -> faxNo !== 'null'){ //if faxNo is given and not null
+            $this -> merge([
+                'fax_no' => json_encode($this -> faxNo)
+            ]);
+        }
     }
 }
