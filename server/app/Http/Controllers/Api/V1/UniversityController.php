7f0f72dae09ea37361f3814ca2faad84ce3a5a9a
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-08 11:43:57 +0530

    create transaction for uni post

--- server/app/Http/Controllers/Api/V1/UniversityController.php
+++ server/app/Http/Controllers/Api/V1/UniversityController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\V1\UpdateUniversityRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\UniversityResource;
 use App\Http\Resources\V1\UniversityCollection;
+use App\Models\CenterForQualityAssurance;
+use Illuminate\Support\Facades\DB;
 
 class UniversityController extends Controller
 {
@@ -24,7 +26,27 @@ class UniversityController extends Controller
      */
     public function store(StoreUniversityRequest $request)
     {
-        return new UniversityResource(University::create($request->validated()));
+        $validatedData = $request -> validated();
+
+        //begin db transaction
+        DB::beginTransaction();
+        try{
+            //create cqa for the university
+            $CQAid = CenterForQualityAssurance::create() -> id; //create an empty cqa and get its id
+            $validatedData['center_for_quality_assurance_id'] = $CQAid;
+            $resource = new UniversityResource(University::create($validatedData));
+            //commit db transactions
+            DB::commit();
+            return $resource;
+        }
+        catch(\Exception $e){
+            //rollback db transactions
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Something went wrong',
+                'error' => $e -> getMessage()
+            ], 500);
+        }
     }
 
     /**
