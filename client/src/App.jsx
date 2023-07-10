117c026cbe0444a37b143dd36271f6f2553ca3c7
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-10 20:00:43 +0530

    add route for created page (not final)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Route, Routes} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Universities from './pages/QACOfficer/Universities';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Dashboard/>}/>
+      <Route path="/qacofficer/universities" element={<Universities/>} />
     </Routes>
   )
 }
