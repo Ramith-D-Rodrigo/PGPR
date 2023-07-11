95ccd471ce83d6d302a3af4f9946582473026d5a
BhathiyaVandebona <malingabhathiya@gmail.com>
2023-07-12 00:34:16 +0530

    Added the laravel:breeze scaffoldings and configured the api using breeze

--- server/routes/api.php
+++ server/routes/api.php
@@ -9,42 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-    //all routes that belong to v1 version of the API will go here
-    //for now, the routes for all the controllers are defined
-    //later we can remove the routes that are not needed
-    Route::apiResource('viceChancellors', 'ViceChancellorController');
-    Route::apiResource('users', 'UserController');
-    Route::apiResource('universitySides', 'UniversitySideController');
-    Route::apiResource('universities', 'UniversityController');
-    Route::apiResource('standards', 'StandardController');
-    Route::apiResource('selfEvaluationReports', 'SelfEvaluationReportController');
-    Route::apiResource('reviewTeams', 'ReviewTeamController');
-    Route::apiResource('reviewers', 'ReviewerController');
-    Route::apiResource('qualityAssuranceStaffs', 'QualityAssuranceStaffController');
-    Route::apiResource('qualityAssuranceCouncilOfficers', 'QualityAssuranceCouncilOfficerController');
-    Route::apiResource('qualityAssuranceCouncilDirectors', 'QualityAssuranceCouncilDirectorController');
-    Route::apiResource('properEvaluations', 'ProperEvaluationController');
-    Route::apiResource('programmeCoordinators', 'ProgrammeCoordinatorController');
-    Route::apiResource('postGraduateProgramReviews', 'PostGraduateProgramReviewController');
-    Route::apiResource('postGraduateProgamReviewApplications', 'PostGraduateProgamReviewApplicationController');
-    Route::apiResource('postGraduatePrograms', 'PostGraduateProgramController');
-    Route::apiResource('internalQualityAssuranceUnitDirectors', 'InternalQualityAssuranceUnitDirectorController');
-    Route::apiResource('internalQualityAssuranceUnits', 'InternalQualityAssuranceUnitsController');
-    Route::apiResource('faculties', 'FacultyController');
-    Route::apiResource('deskEvaluations', 'DeskEvaluationController');
-    Route::apiResource('deans', 'DeanController');
-    Route::apiResource('criterias', 'CriteriaController');
-    Route::apiResource('centerForQualityAssuranceDirectors', 'CenterForQualityAssuranceDirectorController');
-    Route::apiResource('centerForQualityAssurances', 'CenterForQualityAssurancesController');
-    Route::apiResource('academicStaffs', 'AcademicStaffController');
 });
