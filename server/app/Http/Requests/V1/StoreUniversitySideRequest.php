84abb17784e5c68fdf6f79de925fc8f921189ed0
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-12 10:52:33 +0530

    add validation

--- server/app/Http/Requests/V1/StoreUniversitySideRequest.php
+++ server/app/Http/Requests/V1/StoreUniversitySideRequest.php
@@ -3,15 +3,16 @@
 namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class StoreUniversitySideRequest extends FormRequest
+class StoreUniversitySideRequest extends StoreUserRequest //because university side is a user
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +22,18 @@ class StoreUniversitySideRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
-        ];
+        $rulesArr = parent::rules(); //get the rules from the parent class (StoreUserRequest)
+
+        //add the rules for university side
+        $rulesArr['university_id'] = ['required', 'exists:universities,id'];
+        $rulesArr['staff_position'] = ['required', Rule::in('qa', 'academic', 'vc')];
+
+        return $rulesArr;
+    }
+
+    public function prepareForValidation(){
+
+        //all parent class fields are converted to snake case
+        parent::prepareForValidation();
     }
 }
