d53008908b29dd9fb0ef6a44ced160b4285f97ac
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-07-16 06:33:26 +0530

    upgrade main content

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -11,7 +11,7 @@ let drawerWidth = 240;
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
     ({ theme, open }) => ({
       height: '90%',
-      padding: theme.spacing(3),
+      padding: theme.spacing(2),
       transition: theme.transitions.create('margin', {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
@@ -22,8 +22,11 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
           easing: theme.transitions.easing.easeOut,
           duration: theme.transitions.duration.enteringScreen,
         }),
-        marginLeft: 0,
+        margin: "80px 15px 15px 15px",
+        borderRadius:5,
       }),
+      backgroundColor:'white',
+  boxShadow: !open? "none" : "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
     }),
   );
 
@@ -32,21 +35,29 @@ const MainContent = ({open, drawerWidthInput, content}) => {
     return (
         <Box sx={{display: 'flex', flexDirection: 'column', width: '100%', position: 'relative', minHeight:'100vh',}}>  
             <Main open={open}>
-                <DrawerHeader/>
+                {!open && <DrawerHeader/>}
                 {content} {/* content according to the page */}
-{  /*            
-                <Grid container spacing={3}>
-                <Card 
-                  title={'Requested Applications'}
-                  content={'2023 Mar 30 - 2023 Jult 30'}
-                  message={'Ends in 4 Days'}
-                />
-                <Card 
-                  title={'Requested Applications'}
-                  content={'2023 Mar 30 - 2023 Jult 30'}
-                  message={'Ends in 4 Days'}/>
+{              
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                  
+                  <Grid item>
+                    <Card 
+                    title={'Requested Applications'}
+                    content={'2023 Mar 30 - 2023 Jult 30'}
+                    message={'Ends in 4 Days'}
+                  />
                   </Grid>
-*/}
+
+                  <Grid item>
+                    <Card 
+                    title={'Requested Applications'}
+                    content={'2023 Mar 30 - 2023 Jult 30'}
+                    message={'Ends in 4 Days'}
+                  />
+                  </Grid>
+                
+                </Grid>
+}
             </Main>         
             <Footer drawerOpen={open} drawerWidthInput={drawerWidthInput}/>
         </Box>
