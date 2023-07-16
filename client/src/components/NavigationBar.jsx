0f0ddf0ffd7b37b365e9b4e4dc4293b405b5a42b
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-07-16 06:31:27 +0530

    Upgrade Navigation bar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -5,6 +5,7 @@ import BreadCrumbs from './BreadCrumbs';
 import {Divider} from '@mui/material';
 import styled from '@mui/material/styles/styled';
 import IconButton from '@mui/material/IconButton';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import MenuIcon from '@mui/icons-material/Menu';
 
 let drawerWidth = 240; //default value
@@ -17,20 +18,24 @@ const AppBar = styled(MuiAppBar, {
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
+    width: `calc(100% - ${drawerWidth+30}px)`,
     marginLeft: `${drawerWidth}px`,
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
+    margin:"5px 15px 0px 0px",
+    borderRadius:5,
   }),
+  backgroundColor:'white',
+  boxShadow: !open? "none" : "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
 }));
 
 //open and openDrawer are for sidebar (navigation bar is a child of sidebar)
 const NavigationBar = ({open , openDrawer, drawerWidthInput, breadCrumbs}) => {
   drawerWidth = drawerWidthInput;
   return (
-      <AppBar color='transparent' style={{boxShadow: 'none'}} open={open} position='absolute'>
+      <AppBar color='transparent' open={open} position='absolute'>
           <Toolbar>
               <IconButton
                 color="inherit"
@@ -43,13 +48,15 @@ const NavigationBar = ({open , openDrawer, drawerWidthInput, breadCrumbs}) => {
               </IconButton>
               {breadCrumbs} {/* breadcrumbs according to the page */}
               <BreadCrumbs/>
-              <Stack direction='row' spacing={2}>
-                  
+              <Stack sx={{ justifyContent: "center", alignItems:"center" }} direction='row' spacing={2}>
+                  <NotificationsIcon/>
                   <Typography variant='h6' color='black'>
                       User&apos;s Name
                   </Typography>
                   <Divider orientation='vertical' flexItem color='black'/>
-                  <Avatar/>
+                  <Avatar
+                    src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80'
+                  />
               </Stack>
           </Toolbar>
       </AppBar>
