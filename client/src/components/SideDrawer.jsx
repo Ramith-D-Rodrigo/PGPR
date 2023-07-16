2bc0b231d4f0dcc7df2278490c33a6b20896c359
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-07-16 07:16:29 +0530

    side drawer styling with a shadow

--- client/src/components/SideDrawer.jsx
+++ client/src/components/SideDrawer.jsx
@@ -24,6 +24,7 @@ const SideDrawer = ({drawerOpen, drawerCloseHandler, drawerWidthInput, userRoute
               width: drawerWidth,
               boxSizing: 'border-box',
             },
+            boxShadow: !open? "none" : "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
           }}
           variant="persistent"
           anchor="left"
