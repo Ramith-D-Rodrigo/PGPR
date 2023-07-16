c626fd9ec50b46f437a243f223fd9155adbdf7ed
TharinduSampath2000 <stharindu444@gmail.com>
2023-07-17 03:17:01 +0530

    Created a table component, But not finalized

--- client/src/pages/QACOfficer/Universities.jsx
+++ client/src/pages/QACOfficer/Universities.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
 import MainLayout from '../../components/MainLayout';
-import axios from 'axios';
-import { SERVER_API_VERSION, SERVER_URL } from '../../assets/constants';
+// import axios from 'axios';
+// import { SERVER_API_VERSION, SERVER_URL } from '../../assets/constants';
+import Table from '../../components/Table';
 
 const Universities = () => {
 
-    axios.get(SERVER_URL + SERVER_API_VERSION + '/universities')
-    .then(res => {
-        console.log(res.data);
-    })
+    // axios.get(SERVER_URL + SERVER_API_VERSION + '/universities')
+    // .then(res => {
+    //     console.log(res.data);
+    // })
 
+    const tableData = [
+        { id: 1, column1Data: 'Data 1', column2Data: 'Data 2' },
+        { id: 2, column1Data: 'Data 3', column2Data: 'Data 4' },
+    ];
 
     return (
-        <MainLayout></MainLayout>
+        <MainLayout>
+            <Table data={tableData} />
+        </MainLayout>
     )
 }
 
