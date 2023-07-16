0b4ff9b4ae4b584cd5a870ead23a8017f77fb33a
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-07-16 06:29:45 +0530

    re-arrange breadcrumbs

--- client/src/components/BreadCrumbs.jsx
+++ client/src/components/BreadCrumbs.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const BreadCrumbs = () => {
   return (
-    <Breadcrumbs aria-label='breadcrumb' sx={{flexGrow: 1}} separator='>'>
+    <Breadcrumbs aria-label='breadcrumb' sx={{flexGrow: 1}} separator='&#x27A4;'>
         <Link to='/'>Dashboard</Link>
         <Link to='/'>Programmes</Link>
         <Link to='/'>Programme</Link>
