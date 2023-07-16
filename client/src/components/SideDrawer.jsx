400ce3cc7cf743a6ce69d27e0f954be6e5511df5
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-07-16 07:10:55 +0530

    create logout button in drawer

--- client/src/components/SideDrawer.jsx
+++ client/src/components/SideDrawer.jsx
@@ -3,7 +3,8 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import HelpIcon from '@mui/icons-material/Help';
-import { Avatar } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Avatar,Typography } from '@mui/material';
 import DrawerHeader from './DrawerHeader';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -28,10 +29,10 @@ const SideDrawer = ({drawerOpen, drawerCloseHandler, drawerWidthInput, userRoute
           anchor="left"
           open={drawerOpen}
         >
-          <IconButton sx={{position:"fixed",width:"fit-content",left:"5px",top:"5px",}} onClick={drawerCloseHandler}>
+          <IconButton sx={{position:"fixed",width:"fit-content",left:"10px",top:"10px",}} onClick={drawerCloseHandler}>
                 <MenuIcon />
           </IconButton> 
-          <IconButton sx={{position:"fixed",width:"fit-content",left:`${drawerWidth-45}px`,top:"5px",}} onClick={() => {alert("Welcome to User Guide")}}>
+          <IconButton sx={{position:"fixed",width:"fit-content",left:`${drawerWidth-45}px`,top:"10px",}} onClick={() => {alert("Welcome to User Guide")}}>
                 <HelpIcon/>
           </IconButton> 
           <DrawerHeader sx={{justifyContent:"center",padding:"10px"}}>
@@ -41,7 +42,9 @@ const SideDrawer = ({drawerOpen, drawerCloseHandler, drawerWidthInput, userRoute
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8G70_aXsPvet0K-TvhODLgf6P8eJVQ2iL_ZsmDx5Lm9eK7C_9pWXAg3dfmtaTjad0xZM&usqp=CAU"
             />
           </DrawerHeader>
+
           <Divider variant="middle" color='black'/>
+          
           {userRoutes} {/* routes according to the user role */}
           <List component="nav" aria-label="mailbox folders">
 
@@ -62,7 +65,21 @@ const SideDrawer = ({drawerOpen, drawerCloseHandler, drawerWidthInput, userRoute
             <ListItem button divider>
               <ListItemText primary="Spam" />
             </ListItem>
-        </List>
+
+          </List>
+
+          <IconButton onClick={()=>{alert("Log Out")}}sx={{
+          position:"fixed",width:"fit-content",left:"0px",bottom:"10px",
+          }}
+          >
+              <LogoutIcon
+              titleAccess='Log Out'
+              sx={{width:35,height:35,}}
+              />
+              {/* <Typography gutterBottom variant='body1' component='div'>
+                Log Out
+              </Typography> */}
+          </IconButton>
         </Drawer>
     );
 }
