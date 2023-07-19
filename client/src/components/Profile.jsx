70cb55aa7c6f24d4764334684f5b9aa2ef615bd2
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-19 06:02:20 +0530

    Added a GET request for user data
    Will handle user data fetching after authentication

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,13 +1,34 @@
-import useAuth from "../hooks/useAuth";
+import axios from '../api/api.js';
+import useAuth from "../hooks/useAuth.js";
+import {useEffect, useState} from "react";
 
 const Profile = () => {
-  const { auth } = useAuth();
-  console.log(auth);
-  return (
-    <section>
-      <p>{JSON.stringify(auth)}</p>
-    </section>
-  );
+    const {auth} = useAuth();
+
+    const [user, setUser] = useState({});
+
+    async function getUserData() {
+        try {
+            let response = await axios.get('/api/user');
+            console.log(response);
+            setUser(response?.data);
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        getUserData();
+    }, []);
+
+    console.log(auth);
+
+    return (
+        <section>
+            <p>{JSON.stringify(user)}</p>
+        </section>
+    );
 };
 
 export default Profile;
