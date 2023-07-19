d96c90ca41fe1b69cb564cc895120df545b56669
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-19 05:56:03 +0530

    Added the protected routes, general user routes, and added the persistence components
    Please look at the changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,56 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Login from "./components/Login";
 import Landing from "./components/Landing";
 import Profile from "./components/Profile";
+import Home from "./components/Home";
+import Reviewer from "./components/Reviewer";
 import Authenticate from "./components/Authenticate";
 import Unauthorized from "./components/Unauthorized";
 import PageLayout from "./layouts/PageLayout";
+import LoginPersist from "./components/LoginPersist.jsx";
 import "./App.css";
 
 function App() {
-  return (
-    // make changes as needed
-    <Routes>
-      <Route element={<PageLayout />}>
-        <Route path="/">
-          {/* guest routes */}
-          <Route path="/" element={<Login />} />
-          <Route path="login" element={<Login />} />
-          <Route path="landing" element={<Landing />} />
-          {/* protected routes */}
-          <Route element={<Authenticate allowedRoles={["user"]} />}>
-            {/* general user routes */}
-            <Route path="profile" element={<Profile />} />
-            {/* reviewer routes */}
-            <Route
-              element={<Authenticate allowedRoles={["reviewer"]} />}
-            ></Route>
-            {/* QACOfficer routes */}
-            <Route
-              element={<Authenticate allowedRoles={["QACOfficer"]} />}
-            ></Route>
-            {/* and so on */}
-            <Route path="unauthorized" element={<Unauthorized />}></Route>
-          </Route>
-        </Route>
-      </Route>
-    </Routes>
-  );
+    return (
+        // make changes as needed
+        <Routes>
+            <Route element={<PageLayout/>}>
+                <Route path="/">
+                    {/* guest routes */}
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="landing" element={<Landing/>}/>
+                    <Route path="unauthorized" element={<Unauthorized/>}></Route>
+
+                    <Route element={<LoginPersist/>}>
+
+                        {/* protected routes */}
+                        <Route element={<Authenticate allowedRoles={["user", "reviewer", "qac"]}/>}>
+                            {/* general user routes */}
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="home" element={<Home/>}/>
+                            <Route path="profile" element={<Profile/>}/>
+                        </Route>
+
+
+                        <Route
+                            element={<Authenticate allowedRoles={["dean"]}/>}
+                        >
+                            {/* reviewer routes */}
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="reviewer" element={<Reviewer/>}></Route>
+                        </Route>
+
+
+                        <Route
+                            element={<Authenticate allowedRoles={["qac"]}/>}
+                        >
+                            {/* QACOfficer routes */}
+                        </Route>
+                    </Route>
+                </Route>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
