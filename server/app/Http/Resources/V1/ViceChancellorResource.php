16579dc37593f80a618654f5204e0675b5de9136
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-20 17:03:40 +0530

    create resource for vc

---
+++ server/app/Http/Resources/V1/ViceChancellorResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
+
+class ViceChancellorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $returnArr = [];
+        $objProps = $this -> getAttributes();
+
+        foreach($objProps as $key => $value){
+            if($key === 'created_at' || $key === 'updated_at'){
+                continue;
+            }
+            else{
+                //convert to camel case
+                $returnArr[Str::camel($key)] = $value;
+            }
+        }
+
+        return $returnArr;
+    }
+}
