c2d142a02c36f2fa114c61128863f307747209ae
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-21 21:19:56 +0530

    Update StorePostGraduateProgramReviewApplicationRequest.php

--- server/app/Http/Requests/V1/StorePostGraduateProgramReviewApplicationRequest.php
+++ server/app/Http/Requests/V1/StorePostGraduateProgramReviewApplicationRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class StorePostGraduateProgramReviewApplicationRequest extends FormRequest
 {
@@ -22,7 +24,14 @@ class StorePostGraduateProgramReviewApplicationRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
+        $deanFacultyID = null;
+        if(Auth::user()){
+            $dean = Auth::user();
+            //get dean's faculty id from relationships as dean is user model
+            $deanFacultyID = $dean -> universitySide -> academicStaff -> dean -> faculty -> id;
+        }
+
+        $returnArr = [
             'year_1' => ['required', 'string'],
             //year_2 should be greater than year_1
             'year_2' => ['required', 'string', 'gt:year_1'],
@@ -34,21 +43,33 @@ class StorePostGraduateProgramReviewApplicationRequest extends FormRequest
             'year_5' => ['required', 'string', 'gt:year_4'],
             //y_end should be greater than year_5
             'y_end' => ['required', 'date'],
-            'post_graduate_program_id' => ['required', 'integer', 'exists:post_graduate_programs,id'],
-
-            //dean id is taken from the session, moreover post graduate program id should be an id that belongs to faculty of the dean (later this will be added)
+            'dean_id' => ['required', 'integer', 'exists:deans,id'],
         ];
+        
+        $returnArr['post_graduate_program_id'] =
+            ['required', 'integer', Rule::exists('post_graduate_programs', 'id') -> where(function($query) use ($deanFacultyID){
+                $query -> where('faculty_id', $deanFacultyID);
+            })];
+
+        return $returnArr;
     }
 
     public function prepareForValidation(){
         //convert camel case keys to snake case
         $objProps = $this -> all();
 
+        $returnArr = [];
+
         foreach($objProps as $key => $value){
-            $this -> merge([
-                Str::snake($key) => $value
-            ]);
+            $returnArr[Str::snake($key)] = $value;
         }
+
+        $this -> replace($returnArr);
+
+        //get the dean id from the session
+        $this -> merge([
+            'dean_id' => Auth::id() ?? null,
+        ]);
     }
 
     public function messages(){
