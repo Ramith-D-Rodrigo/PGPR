fb568acc23abc06f771df8d17f64bcd5b7f69019
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-22 13:58:32 +0530

    new password reset controller for resetting the password on the initial login

--- server/app/Http/Controllers/Auth/ResetPasswordController.php
+++ server/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,49 +3,36 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Events\PasswordReset;
+use App\Http\Requests\Auth\PasswordResetRequest;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
-use Illuminate\Support\Str;
-use Illuminate\Validation\Rules;
 use Illuminate\Validation\ValidationException;
 
-class RestPasswordController extends Controller
+class ResetPasswordController extends Controller
 {
     // create the initial login password change case in here
     /**
      * @throws ValidationException
      */
-    public function store(Request $request): JsonResponse
+    public function store(PasswordResetRequest $request): JsonResponse
     {
-        // validations
-        $request->validate([
-            'official_email' => ['required', 'email'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        // echo json_encode($request->only('official_email')['official_email']);
+        // echo json_encode($request->only('password')['password']);
+        // exit;
 
-        // change the password
-        $status = Password::reset(
-            $request->only('official_email', 'password', 'password_confirmation'),
-            function ($user) use ($request) {
-                $user->forceFill([
-                    'password' => Hash::make($request->password),
-                    // 'remember_token' => Str::random(60),
-                ])->save();
+//        $user = User ($request->only('official_email')['official_email'])->getFirst();
+        // echo json_encode(Auth::user()->password);
+        // exit;
 
-                event(new PasswordReset($user));
-            }
-        );
-
-        if ($status != Password::PASSWORD_RESET) {
-            throw ValidationException::withMessages([
-                'official_email' => [__($status)],
-            ]);
+        // changing the user password
+        Auth::user()->password = Hash::make($request->only('password')['password']);
+        if (Auth::user()->password) {
+            Auth::user()->logins = Auth::user()->logins + 1;
+            Auth::user()->save();
+            return response()->json(["message" => "Password Successfully Changed"], 201);
         }
-
-        return response()->json(['status' => __($status)]);
+        return response()->json(["error" => "Password cannot be changed"], 422);
     }
 
 }
