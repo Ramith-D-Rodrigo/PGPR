d157bd85ba0c0c00b160e943ebda72ade5c5d71a
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-23 13:14:50 +0530

    Create InternalQualityAssuranceUnitDirectorService.php

---
+++ server/app/Services/V1/InternalQualityAssuranceUnitDirectorService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\V1;
+
+use App\Models\QualityAssuranceStaff;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\InternalQualityAssuranceUnitDirector;
+
+
+class InternalQualityAssuranceUnitDirectorService extends QualityAssuranceStaffService {
+
+    public static function create(array $validateData) : Model {
+        //make sure validated data has status field
+        $qualityAssuranceStaff = parent::create($validateData); //call the parent create function to create the quality assurance staff model
+
+        $internalQualityAssuranceUnitDirector = new InternalQualityAssuranceUnitDirector();
+        $internalQualityAssuranceUnitDirector -> id = $qualityAssuranceStaff -> id;
+        $internalQualityAssuranceUnitDirector -> fill($validateData);
+
+        $internalQualityAssuranceUnitDirector -> save();
+
+        return $internalQualityAssuranceUnitDirector; //return the internal quality assurance unit director object and the password (password for sending the email)
+    }
+}
