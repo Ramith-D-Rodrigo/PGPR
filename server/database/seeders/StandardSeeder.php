b9379225929fabde2a8d766ce1db9f249aed6843
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-23 14:46:07 +0530

    Update StandardSeeder.php

--- server/database/seeders/StandardSeeder.php
+++ server/database/seeders/StandardSeeder.php
@@ -12,6 +12,19 @@ class StandardSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //get all functions of standard factory and call them using for loop
+        $standardFactory = new \Database\Factories\StandardFactory();
+        $standardFactoryMethods = get_class_methods($standardFactory);
+
+
+        foreach ($standardFactoryMethods as $method) {
+            //skip definition method
+            if ($method == "definition") {
+                continue;
+            }
+
+            //call the method
+            $standardFactory->$method()->create();
+        }
     }
 }
