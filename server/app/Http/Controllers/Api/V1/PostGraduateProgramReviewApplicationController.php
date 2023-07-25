ee95f3fa26026af1919bb6e22ed6cbed41bf0e54
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-25 13:06:37 +0530

    qac approval creating a pgpr entry

--- server/app/Http/Controllers/Api/V1/PostGraduateProgramReviewApplicationController.php
+++ server/app/Http/Controllers/Api/V1/PostGraduateProgramReviewApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Resources\V1\PostGraduateProgramReviewApplicationCollection;
+use App\Models\PostGraduateProgramReview;
 use App\Models\PostGraduateProgramReviewApplication;
 use App\Http\Requests\V1\StorePostGraduateProgramReviewApplicationRequest;
 use App\Http\Requests\V1\UpdatePostGraduateProgramReviewApplicationRequest;
@@ -10,6 +11,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\PostGraduateProgramReviewApplicationResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 class PostGraduateProgramReviewApplicationController extends Controller
@@ -168,9 +170,20 @@ class PostGraduateProgramReviewApplicationController extends Controller
                 return response()->json(['message' => 'This post graduate program review application is already approved.'], 400);
             }
 
+            DB::beginTransaction();
+
             $pgprApplication -> update(['quality_assurance_council_officer_id' => $qacOfficerID]);
+
+            //after approving, create a post graduate program review
+            $pgpr = PostGraduateProgramReview::create([
+                'post_graduate_program_id' => $pgprApplication -> post_graduate_program_id,
+                'pgpr_application_id' => $pgprApplication -> id
+            ]);
+
+            DB::commit();
         }
         catch(\Exception $e){
+            DB::rollBack();
             return response()->json(['message' => 'Error approving post graduate program review application.',
                 'error' => $e->getMessage()]
             , 400);
