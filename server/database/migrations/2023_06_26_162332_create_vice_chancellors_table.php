f9c81f67ea7ac11633f16897a2f67a9d46b59e44
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-29 19:19:52 +0530

    university id wasn't there in this branch updated it

--- server/database/migrations/2023_06_26_162332_create_vice_chancellors_table.php
+++ server/database/migrations/2023_06_26_162332_create_vice_chancellors_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('vice_chancellors', function (Blueprint $table) {
             $table->id();
             $table->date("appointed_date");
+            $table->foreignId('university_id')->nullable();
             $table->date("term_date"); // appointed_date + 5 year
             $table->enum("status", ["ACTIVE", "INACTIVE"]);
             $table->timestamps();
