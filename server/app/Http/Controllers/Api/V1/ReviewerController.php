6059a8af0a7c52eebecbd8acbdc054ca80a84e23
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-30 22:26:39 +0530

    added the functions for downloading the reviewer declaration, accepting the role of a reviewer, and rejecting the assigned role

--- server/app/Http/Controllers/Api/V1/ReviewerController.php
+++ server/app/Http/Controllers/Api/V1/ReviewerController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Requests\V1\UpdateAcceptAppointmentRequest;
 use App\Http\Requests\V1\StoreAcademicStaffRequest;
+use App\Http\Requests\V1\UpdateRejectAppointmentRequest;
+use App\Mail\RejectReviewerRole;
 use App\Models\Reviewer;
 use App\Http\Requests\V1\StoreReviewerRequest;
 use App\Http\Requests\V1\UpdateReviewerRequest;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\V1\ReviewerImport;
 use Illuminate\Validation\ValidationException as ValidationValidationException;
@@ -22,18 +36,10 @@ class ReviewerController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     //import reviewers using excel file
     public function importReviewers()
     {
-        try{
+        try {
             Excel::import(new ReviewerImport, request()->file('file'));
             //$arr = Excel::toArray(new ReviewerImport, request()->file('file'));
 
@@ -41,25 +47,162 @@ class ReviewerController extends Controller
             return response()->json([
                 'message' => 'Reviewers imported successfully'
             ], 200);
-        }
-        catch(\Google\Service\Exception $e){ //google drive error
-            return response() -> json([
+        } catch (\Google\Service\Exception $e) { //google drive error
+            return response()->json([
                 'message' => 'Error occurred while importing reviewers',
-                'error' => $e -> getErrors(),
+                'error' => $e->getErrors(),
             ], 500);
-        }
-        catch(ValidationException $e){
-            $failures = $e -> errors();
+        } catch (ValidationException $e) {
+            $failures = $e->errors();
             return response()->json([
                 'errors' => $failures
             ], 422);
-        }
-        catch(ValidationValidationException $e){ //validation errors of the array of rows when validating
+        } catch (ValidationValidationException $e) { //validation errors of the array of rows when validating
             return response()->json([
                 'message' => 'Error occurred while importing reviewers',
-                'error' => $e -> errors(),
+                'error' => $e->errors(),
             ], 500);
         }
+    }
+
+    /**
+     * Downloading the appointment declaration letter
+     */
+    public function downloadDeclarationLetter()
+    {
+        $headers = [
+            "Content-Type: application/octet-stream",
+            "Content-Disposition: attachment; filename=\"Declaration.docx\""
+        ];
+
+        $path = "reviewer_role_declaration/Declaration.docx";
+
+        return response()->download(Storage::disk('public')->path($path), 'Declaration.dox', $headers);
+    }
+
+    /**
+     * Accept appointment
+     */
+    public function acceptAppointment(UpdateAcceptAppointmentRequest $request): Response
+    {
+        try {
+            //get the file
+            $file = $request->file('file');
+
+            // find the reviewer
+            $reviewer = Reviewer::findOrFail(Auth::user()->id);
+
+            if (!$reviewer) {
+                return response("User has invalid credentials.", 401);
+            }
+
+            $candidateID = Auth::id();
+
+            //move the file and create the file name
+            $fileName = Str::random(10) . "_" . $candidateID . "." . $file->getClientOriginalExtension();
+            $path = $file->storeAs('reviewer_declaration_letters', $fileName,  'public');
+
+            //add the file path to the reviewer declaration_letter_path as an url
+            $reviewer->path_to_declaration = Storage::disk('public')->url($path);
+            $reviewer->reviewer_status = 'accepted';
+            $reviewer->save();
+
+            return response("Your declaration letter was successfully submitted.", 200);
+        } catch (Exception $exception) {
+            return response("An internal server error occurred, file cannot be stored.", 500);
+        }
+    }
+
+    /**
+     * Reject appointment
+     */
+    public function rejectAppointment(UpdateRejectAppointmentRequest $request)
+    {
+        try {
+            //get the reviewer
+            $reviewer = Reviewer::findOrFail(Auth::user()->id);
+
+            if (!$reviewer) {
+                return response("User has invalid credentials.", 401);
+            }
+
+            //get user who created the reviewer account
+            $createdUser = User::findOrFail(Auth::user()->created_by);
+            if (!$createdUser) {
+                throw new Exception("This shouldn't happen when creating users");
+            }
+
+            //mail the remark if exists the remark
+            Mail::to($createdUser->official_email)
+                ->send(
+                    new RejectReviewerRole(
+                        $createdUser,
+                        Auth::user(),
+                        $request->validated('remark') ?? NULL,
+                        'User Rejected an Appointment for a Reviewer Role',
+                        'mail.userRejectedReviewerRole'
+                    )
+                );
+
+            //remove access privileges from user
+            $reviewer->reviewer_status = 'rejected';
+
+            //should the reviewer role be removed?
+
+            $reviewer->save();
+
+            return response("Your request was duly noted, thank you for responding.");
+        } catch (Exception $exception) {
+            return response("An internal server error occurred, user request cannot be full filled.", 500);
+        }
+    }
+
+    /**
+     * Use case 1.1
+     * Browse assigned program reviews => with filtering
+     */
+    public function browsePGPRs()
+    {
+    }
+
+    /**
+     * Use case 1.2
+     * View specific program review
+     */
+    public function viewSpecificPGPR()
+    {
+    }
+
+    /**
+     * Use case 1.2.1
+     * Update remarks of Section A, B and D
+     */
+    public function updateRemarksOfSection()
+    {
+    }
+
+    /**
+     * Use case 1.3
+     * Conduct desk evaluation
+     */
+    public function conductDeskEvaluation()
+    {
+    }
+
+    /**
+     * Use case 1.4
+     * View own DE => criteria wise
+     */
+    public function viewOwnDECriteria()
+    {
+    }
+
+    /**
+     * Use case 1.6
+     * View summary of DE grade
+     */
+    public function viewSummaryOfDEGrade()
+    {
     }
 
     /**
@@ -67,21 +210,13 @@ class ReviewerController extends Controller
      */
     public function store(StoreReviewerRequest $request)
     {
-        //
+        // incase the user wants to create a reviewer account
     }
 
     /**
      * Display the specified resource.
      */
     public function show(Reviewer $reviewer)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Reviewer $reviewer)
     {
         //
     }
