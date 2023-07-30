a89b90e594a960185d604233c443e45285c58ccf
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-30 22:35:25 +0530

    added new route end points to support the newly added functionalities

--- server/routes/api.php
+++ server/routes/api.php
@@ -32,26 +32,26 @@ Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], f
     //for now, the routes for all the controllers are defined
     //later we can remove the routes that are not needed
 
+
     //TODO: Renamed route names should reflect on the method names in the controllers
     Route::apiResource('viceChancellors', 'ViceChancellorController');
     Route::apiResource('users', 'UserController');
     Route::apiResource('universitySides', 'UniversitySideController');
-    Route::apiResource('universities', 'UniversityController') -> middleware('auth');
+    Route::apiResource('universities', 'UniversityController')->middleware('auth');
     Route::apiResource('standards', 'StandardController');
     Route::apiResource('selfEvaluationReports', 'SelfEvaluationReportController');
     Route::apiResource('reviewTeams', 'ReviewTeamController');
-    Route::apiResource('reviewers', 'ReviewerController');
     Route::apiResource('qualityAssuranceStaffs', 'QualityAssuranceStaffController');
     Route::apiResource('qacOfficers', 'QualityAssuranceCouncilOfficerController');
     Route::apiResource('qacDirectors', 'QualityAssuranceCouncilDirectorController');
     Route::apiResource('properEvaluations', 'ProperEvaluationController');
     Route::apiResource('programmeCoordinators', 'ProgrammeCoordinatorController');
     Route::apiResource('postGraduateProgramReviews', 'PostGraduateProgramReviewController');
-    Route::apiResource('pgprApplications', 'PostGraduateProgramReviewApplicationController') -> middleware('auth');
-    Route::apiResource('postGraduatePrograms', 'PostGraduateProgramController') -> middleware('auth');
+    Route::apiResource('pgprApplications', 'PostGraduateProgramReviewApplicationController')->middleware('auth');
+    Route::apiResource('postGraduatePrograms', 'PostGraduateProgramController')->middleware('auth');
     Route::apiResource('iqauDirectors', 'InternalQualityAssuranceUnitDirectorController');
     Route::apiResource('iqaUnits', 'InternalQualityAssuranceUnitController');
-    Route::apiResource('faculties', 'FacultyController') -> middleware('auth');
+    Route::apiResource('faculties', 'FacultyController')->middleware('auth');
     Route::apiResource('deskEvaluations', 'DeskEvaluationController');
     Route::apiResource('deans', 'DeanController');
     Route::apiResource('criterias', 'CriteriaController');
@@ -60,12 +60,23 @@ Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], f
     Route::apiResource('academicStaffs', 'AcademicStaffController');
 
     //route for reviewer import from excel
-    Route::post('reviewers/import', 'ReviewerController@importReviewers');
+    Route::post('reviewers/import', 'ReviewerController@importReviewers')->middleware('auth');
+
+    //ROUTES OF THE REVIEWER ACTOR
+    //get the appointment declaration
+    Route::get('reviewers/download-declaration', 'ReviewerController@downloadDeclarationLetter')->middleware('auth');
+    //upload the declaration letter
+    Route::post('reviewers/accept-appointment', 'ReviewerController@acceptAppointment')->middleware('auth');
+    //valid declaration must be sent
+    Route::post('reviewers/reject-appointment', 'ReviewerController@rejectAppointment')->middleware('auth');
+
+    // api resource => this must come here otherwise the declaration doc will have problems
+    Route::apiResource('reviewers', 'ReviewerController');
 
     //route for google drive file info (for now, only the metadata is returned (testing))
     Route::post('driveFileInfo', 'GoogleDriveController@getFileInfo');
     Route::get('downloadFile', 'GoogleDriveController@downloadFile');
 
     //other routes for pgpr application
-    Route::post('pgprApplications/{pgprApplication}/submit', 'PostGraduateProgramReviewApplicationController@submit') -> middleware('auth');  //submit pgpr application by the dean
+    Route::post('pgprApplications/{pgprApplication}/submit', 'PostGraduateProgramReviewApplicationController@submit')->middleware('auth');  //submit pgpr application by the dean
 });
