e30e8e21a243f7862066d13a7c937168623bd97f
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-30 22:29:12 +0530

    validations for rejecting reviewer role by an appointed user

--- server/app/Http/Requests/V1/UpdateRejectAppointmentRequest.php
+++ server/app/Http/Requests/V1/UpdateRejectAppointmentRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\V1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateRejectAppointmentRequest extends FormRequest
@@ -11,18 +12,27 @@ class UpdateRejectAppointmentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        //should be done in the early stages, before becoming an actual reviewer
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            // this is optional
+            'remark' => 'nullable|max:255',
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'remark' => $this->reasonForRejecting,
+        ]);
     }
 }
