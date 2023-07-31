81ed76edfd58dfcd1c604575755fdd0aa3074413
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-07-31 11:34:54 +0530

    formatted the file and added the status column

--- server/database/migrations/2023_06_28_083532_create_review_teams_table.php
+++ server/database/migrations/2023_06_28_083532_create_review_teams_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,32 +12,33 @@ return new class extends Migration
     {
         Schema::create('review_teams', function (Blueprint $table) {
             $table->id();
-            $table -> unsignedBigInteger('quality_assurance_council_officer_id'); //qac officer who created the review team
-            $table -> foreignId('pgpr_id');
-            $table -> unsignedBigInteger('dean_id'); //dean who gives the consent
-            $table -> string('dean_decision');
-            $table -> string('remarks');
+            $table->unsignedBigInteger('quality_assurance_council_officer_id'); //qac officer who created the review team
+            $table->foreignId('pgpr_id');
+            $table->unsignedBigInteger('dean_id'); //dean who gives the consent
+            $table->enum('status', ['PENDING', 'ACCEPTED', 'REJECTED']); //in case the deans rejected the review team
+            $table->string('dean_decision');
+            $table->string('remarks');
             $table->timestamps();
 
             // indices
             $table->index('pgpr_id');
 
             //foreign key
-            $table -> foreign('quality_assurance_council_officer_id') -> references('id') -> on('quality_assurance_council_officers');
-            $table -> foreign('dean_id') -> references('id') -> on('deans');
+            $table->foreign('quality_assurance_council_officer_id')->references('id')->on('quality_assurance_council_officers');
+            $table->foreign('dean_id')->references('id')->on('deans');
         });
 
         //create reviewer_review_teams pivot table
         Schema::create('reviewer_review_teams', function (Blueprint $table) {
             $table->id();
-            $table -> unsignedBigInteger('reviewer_id');
-            $table -> unsignedBigInteger('review_team_id');
-            $table -> enum('role', ['CHAIR', 'MEMBER']);
+            $table->unsignedBigInteger('reviewer_id');
+            $table->unsignedBigInteger('review_team_id');
+            $table->enum('role', ['CHAIR', 'MEMBER']);
             $table->timestamps();
 
             //foreign key
-            $table -> foreign('reviewer_id') -> references('id') -> on('reviewers');
-            $table -> foreign('review_team_id') -> references('id') -> on('review_teams');
+            $table->foreign('reviewer_id')->references('id')->on('reviewers');
+            $table->foreign('review_team_id')->references('id')->on('review_teams');
         });
     }
 
