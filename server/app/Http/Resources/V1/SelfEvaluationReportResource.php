ea9652ab14077cfb1c22abb2d73bba81a318ff3d
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-08-01 16:34:31 +0530

    fix resource

--- server/app/Http/Resources/V1/SelfEvaluationReportResource.php
+++ server/app/Http/Resources/V1/SelfEvaluationReportResource.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Models\Criteria;
 use App\Models\PostGraduateProgram;
 use App\Models\ProgrammeCoordinator;
 use App\Models\Standard;
+use App\Services\V1\StandardService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Str;
@@ -28,28 +30,29 @@ class SelfEvaluationReportResource extends JsonResource
             }
 
             //convert to camel case
-            if($key === 'post_graduate_program_review_id'){ //prepend the PGPR- to value (prefix)
-                $returnArr[Str::camel($key)] = 'PGPR-'. $key;
+            if($key === 'post_graduate_program_review_id'){ //prepend the PGPR- to value (prefix) (moved this to pgpr resource)
+                continue;
             }
             else{
                 $returnArr[Str::camel($key)] = $value;
             }
         }
 
+        //lazy loaded relations
+        $returnArr['postGraduateProgramReview'] = new PostGraduateProgramReviewResource($this -> whenLoaded('postGraduateProgramReview'));
+
         //find the standards that are applicable to this SER of the pgp
-
-        //get the pgp
-        $pgp = ProgrammeCoordinator::find(['id' => $objProps['pgp_coordinator_id']]) -> postGraduateProgram;
-
-        //get the slql level
-        $slqf_level = $pgp -> slqf_level;
-
+        $slqf_level = $returnArr['postGraduateProgramReview'] -> postGraduateProgram -> slqf_level;
         //get all the standards that have this slqf level (or have the 'all' option)
-        $standards = Standard::whereJsonContains('valid_slqf_levels', ['all', $slqf_level]) -> get();
+        $standards = StandardService::getApplicableStandards($slqf_level, false);
 
+        //load the criteria and standards that is applicable to this SER
+        $returnArr['criterias'] = new CriteriaCollection(Criteria::with(['standards' => function($query) use ($standards){
+            $query -> whereIn('id', array_column($standards, 'id'));
+        }]) -> get());
 
-        $returnArr['standards'] = new StandardCollection($standards);
-
+        //load the standards that have given evidence
+        $returnArr['evidenceGivenStandards'] = new StandardCollection($this -> whenLoaded('adherenceToStandards'));
         return $returnArr;
     }
 }
