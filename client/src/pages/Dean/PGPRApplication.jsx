0c849671dcbe88cc299f7373a8428335a23cb046
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-08-02 09:46:16 +0530

    feat:change slqf level when pgp selected

--- client/src/pages/Dean/PGPRApplication.jsx
+++ client/src/pages/Dean/PGPRApplication.jsx
@@ -8,27 +8,27 @@ import { useState, useEffect, useRef } from 'react';
 
 const PGPRApplication = () => {
   const [pgps, setPGPs] = useState([]); //state variable to store the response data
+  // const [selectedPGP, setSelectedPGP] = useState("");
+  const [selectedSLQFLevel, setSelectedSLQFLevel] = useState("");
 
   let selectedPGP = useRef("");
-  let selectedSLQFLevel = useRef("");
+  // let selectedSLQFLevel = useRef("");
 
   useEffect(() => {
     getPostGraduateProgrammes().then((res) => {
       setPGPs(res.data.data);
       console.log(res.data.data); //data is an array of objects
-    })
-    .then(() => {
-      selectedPGP.current = pgps[0];
-      selectedSLQFLevel.current = selectedPGP.current.slqfLevel;
 
-      displaySLQFLevelForPGP({target: {value: selectedPGP.id}});
+      //set the default selc=ected pgp & SLQF level to 0th index of the array
+      selectedPGP.current = res.data.data[0];
+      setSelectedSLQFLevel(res.data.data[0]?.slqf_level);
     }) ;
   }, []);
 
 
   const displaySLQFLevelForPGP = (event) => {
-    selectedPGP.current = pgps.find((pgp) => pgp.id === event.target.value);
-    selectedSLQFLevel.current = selectedPGP.current.slqfLevel;
+    selectedPGP.current = pgps.find((pgp) => pgp.id === parseInt(event.target.value));
+    setSelectedSLQFLevel(selectedPGP?.current?.slqf_level);
   }
 
 
@@ -42,7 +42,7 @@ const PGPRApplication = () => {
                 onchange: displaySLQFLevelForPGP,
                 },
                 {label: "Professional Postgraduate Programme", name: "isProfessionalDegreeProgramme", type: "checkbox", isReadonly: true, options: [{label: "Yes", value: "1"}]},
-                {label: "SLQF Level", name: "slqfLevel", type: "text", isReadonly: true, value:{selectedSLQFLevel}},
+                {label: "SLQF Level", name: "slqfLevel", type: "text", isReadonly: true, value:selectedSLQFLevel},
                 /*years should be a list of years*/
                 {label: "Year 1", name: "year_1", type: "select", isReadonly: false, options: [{label: "Semester 1", value: "1"}, {label: "Semester 2", value: "2"}]},
                 {label: "Year 2", name: "year_2", type: "select", isReadonly: false, options: [{label: "Semester 1", value: "1"}, {label: "Semester 2", value: "2"}]},
