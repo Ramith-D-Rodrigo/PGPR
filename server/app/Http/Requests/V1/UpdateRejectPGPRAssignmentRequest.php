2addf2115eb8dfe1c083c5e4773eb1ada1feda92
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-08-02 17:48:06 +0530

    new request validator to validate the incoming data for an update state request regarding a reviewers rejection of a PGPR

--- server/app/Http/Requests/V1/UpdateRejectPGPRAssignmentRequest.php
+++ server/app/Http/Requests/V1/UpdateRejectPGPRAssignmentRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\V1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateRejectPGPRAssignmentRequest extends FormRequest
@@ -17,12 +18,31 @@ class UpdateRejectPGPRAssignmentRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'pgpr_id' => 'required|exists:post_graduate_program_reviews,id',
+            'comment' => 'required|min:1|max:255'//the maximum file size is 4MB
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'pgpr_id' => $this->pgprID
+        ]);
+    }
+
+    public function messages()
+    {
+        return [
+            'pgpr_id.required' => "Need to send the ID of the post graduate program in order to process the request.",
+            'pgpr_id.exists' => "There is no such a post graduate program in our database.",
+            'comment.required' => "You need to provide a reason for rejecting this assignment.",
+            'comment.min' => "The reason you provide must at least contain a single word.",
+            'comment.max' => "The reason that you provide can only contain at most 255 characters."
         ];
     }
 }
