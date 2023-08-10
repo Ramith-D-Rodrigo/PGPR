5680f3ec01cdfe0029cadafaf642cda0e2b14d14
TharinduSampath2000 <stharindu444@gmail.com>
2023-08-10 18:12:19 +0530

    Fix: prop type validation

--- client/src/contexts/UserNavigationsProvider.jsx
+++ client/src/contexts/UserNavigationsProvider.jsx
@@ -1,16 +1,23 @@
 import { createContext, useState } from "react";
+import { PropTypes } from "prop-types";
 
 // User Navigations context
 const UserNavigationsContext = createContext({});
 
-export const UserNavigationsProvider = ({children}) => {
+export const UserNavigationsProvider = ({ children }) => {
+  UserNavigationsProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
 
-    const [userNavigations, setUserNavigations] = useState([]);
+  const [userNavigations, setUserNavigations] = useState([]);
+
   return (
-    <UserNavigationsContext.Provider value={{userNavigations, setUserNavigations}}>
-        {children}
+    <UserNavigationsContext.Provider
+      value={{ userNavigations, setUserNavigations }}
+    >
+      {children}
     </UserNavigationsContext.Provider>
-  )
-}
+  );
+};
 
 export default UserNavigationsContext;
