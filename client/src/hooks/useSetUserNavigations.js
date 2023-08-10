d383dd90748ffbbdc61d41328125cd25735cc58c
TharinduSampath2000 <stharindu444@gmail.com>
2023-08-10 18:32:46 +0530

    Fix: Added missing dependencies

--- client/src/hooks/useSetUserNavigations.js
+++ client/src/hooks/useSetUserNavigations.js
@@ -1,26 +1,28 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useRef } from "react";
 import UserNavigationsContext from "../contexts/UserNavigationsProvider";
 import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 
-const useSetUserNavigations = (navigations) =>
-{
-    const {setUserNavigations} = useContext(UserNavigationsContext);
-    const {auth} = useAuth();
-    const Role = auth?.authRole[0];
-    const navigate = useNavigate();
+const useSetUserNavigations = (navigations) => {
+  const { setUserNavigations } = useContext(UserNavigationsContext);
+  const { auth } = useAuth();
+  const Role = auth?.authRole[0];
+  const navigate = useNavigate();
+  const navigationsRef = useRef(navigations);
 
-    useEffect(()=>{
-        !Role && navigate('/login');
+  useEffect(() => {
+    !Role && navigate("/login");
 
-        navigations = navigations.map((navigation,index)=>{
-            return {
-                name:navigation.name,
-                link: (String(navigation.link).includes('/'+Role)? '' : '/'+Role)+navigation.link,
-            } 
-        })
-        setUserNavigations(navigations);
-    },[]);
-}
+    navigationsRef.current = navigationsRef.current.map((navigation) => {
+      return {
+        name: navigation.name,
+        link:
+          (String(navigation.link).includes("/" + Role) ? "" : "/" + Role) +
+          navigation.link,
+      };
+    });
+    setUserNavigations(navigationsRef.current);
+  }, [Role, navigate, navigations, setUserNavigations]);
+};
 
 export default useSetUserNavigations;
