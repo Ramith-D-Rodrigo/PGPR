1cfd1a3d985f12a951229b29a1b8e8d6f2e0052d
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-08-12 21:15:16 +0530

    fix issue with professional pg

--- server/app/Http/Controllers/Api/V1/SelfEvaluationReportController.php
+++ server/app/Http/Controllers/Api/V1/SelfEvaluationReportController.php
@@ -66,7 +66,7 @@ class SelfEvaluationReportController extends Controller
             //call the whenLoaded method to load the relations only if they are loaded
             //load the following relations to get the needed details
             'postGraduateProgramReview:id,post_graduate_program_id,pgpr_application_id' => [
-                'postGraduateProgram:id,title,slqf_level,faculty_id,programme_coordinator_id' => [
+                'postGraduateProgram:id,title,slqf_level,faculty_id,programme_coordinator_id,is_professional_pg_programme' => [
                     'faculty:id,name,university_id' => [
                         'university:id,name',
                     ],
