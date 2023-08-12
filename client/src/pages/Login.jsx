d691674cbd6f8eeef43499e9802e88ff2fcb459a
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-08-12 23:32:41 +0530

    chng:login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import {
-  Grid,
   Paper,
   Box,
   Typography,
@@ -21,9 +20,10 @@ import {
   IconButton,
   MenuItem,
   Select,
+  Container
 } from "@mui/material";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import PasswordIcon from "@mui/icons-material/Password";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
@@ -139,7 +139,7 @@ const Login = () => {
   }));
 
   const ColorButton = styled(Button)(({ theme }) => ({
-    width: "300px",
+    // width: "300px",
     margin: "0 0 15px",
     borderRadius: "10px",
     backgroundColor: "#0E3B81",
@@ -159,229 +159,141 @@ const Login = () => {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        height: "100vh",
-        justifyContent: "center",
-      }}
-    >
-      <Grid
-        container
-        spacing={2}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={7} justifyContent={"center"} alignItems={"center"}>
-          <Typography
-            style={{ color: "white" }}
-            variant="h4"
-            gutterBottom
-            textAlign={"center"}
-          >
-            Quality Assurance Framework for <br /> <span>Postgraduate</span>{" "}
-            Programs
-          </Typography>
-          <img
-            style={{ height: "60vh", width: "60%", margin: "auto" }}
-            src={
-              "https://assets-global.website-files.com/5e8b3356a5a8f5321855bbe7/648c7d9b38164fd8fc587f8a_img-person-form.png"
-            }
-            alt="temporary"
-          />
-        </Grid>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            height: "100vh",
-            justifyContent: "center",
-          }}
-        >
-          <Grid
-            container
-            spacing={2}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Paper elevation={10} style={paperStyle}>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  height: "100%",
-                  justifyContent: "center",
-                }}
-              >
-                <Grid align="center">
-                  {/* <Avatar style={avatarStyles}><LockIcon fontSize={"large"}/></Avatar> */}
-                  <Typography style={{ margin: "20px 0" }} variant="h3">
-                    Sign In
-                  </Typography>
+    <Container sx={{display:"flex",alignItems:"center",height:"100vh",justifyContent:"space-around"}}>
 
-                  <form onSubmit={handleLogin}>
-                    <FormControl
-                      style={{ margin: "15px 0" }}
-                      variant="standard"
-                      fullWidth
-                      required
-                    >
-                      {/* <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel> */}
-                      <Input
-                        style={{ margin: "15px 0" }}
-                        id="input-email"
-                        startAdornment={
-                          <InputAdornment
-                            style={{ margin: "15px 10px 20px 0px" }}
-                            position="start"
-                          >
-                            <MailOutlineIcon fontSize="large" />
-                          </InputAdornment>
-                        }
-                        autoFocus
-                        autoComplete="off"
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </FormControl>
+      <Box sx={{display:"flex",alignItems:"center",height:"100vh",justifyContent:"center"}}>
+        
+        <Paper elevation={10} style = {paperStyle}>
+          <Box sx={{display:"flex",flexDirection:'column',alignItems:"center",height:"100%",width:"100%",justifyContent:"center"}}>
+            
+                {/* <Avatar style={avatarStyles}><LockIcon fontSize={"large"}/></Avatar> */}
+                <Typography style={{margin:"20px 0"}} variant="h3">Sign In</Typography>
+                
+                
+                  
+                <form onSubmit={handleLogin}>
+                  <FormControl style={{margin:"15px 0"}} variant="standard" fullWidth
+                      required>
+                    {/* <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel> */}
+                    <Input style={{margin:"15px 0"}}
+                      id="input-email"
+                      startAdornment={
+                        <InputAdornment style={{margin:"15px 10px 20px 0px"}} position="start">
+                          <MailOutlineIcon fontSize='large' />
+                        </InputAdornment>
+                      }
+                      autoFocus
+                      autoComplete="off"
+                      type='email'
+                      placeholder='Email'
+                      value={email}
+                      onChange={(e)=>setEmail(e.target.value)}
+                    />
+                  </FormControl>
 
-                    <FormControl
-                      style={{ margin: "15px 0" }}
-                      variant="standard"
-                      fullWidth
-                      required
-                    >
-                      {/* <InputLabel htmlFor="input-with-icon-adornment">Password</InputLabel> */}
-                      <Input
-                        style={{ margin: "15px 0" }}
-                        id="input-password"
-                        startAdornment={
-                          <InputAdornment
-                            style={{ margin: "15px 10px 20px 0px" }}
-                            position="start"
+                  <FormControl style={{margin:"15px 0"}} variant="standard" fullWidth
+                      required>
+                    {/* <InputLabel htmlFor="input-with-icon-adornment">Password</InputLabel> */}
+                    <Input style={{margin:"15px 0"}}
+                      id="input-password"
+                      startAdornment={
+                        <InputAdornment style={{margin:"15px 10px 20px 0px"}} position="start">
+                          <LockOutlinedIcon fontSize='large' />
+                        </InputAdornment>
+                      }
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
                           >
-                            <PasswordIcon fontSize="large" />
-                          </InputAdornment>
-                        }
-                        endAdornment={
-                          <InputAdornment position="end">
-                            <IconButton
-                              aria-label="toggle password visibility"
-                              onClick={handleClickShowPassword}
-                              onMouseDown={handleMouseDownPassword}
-                            >
-                              {showPassword ? (
-                                <VisibilityOffOutlinedIcon />
-                              ) : (
-                                <VisibilityOutlinedIcon />
-                              )}
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                        placeholder="Password"
-                        type={showPassword ? "text" : "password"}
-                        fontSize="large"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </FormControl>
+                            {showPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      placeholder='Password'
+                      type={showPassword ? 'text' : 'password'}
+                      fontSize='large'
+                      value={password}
+                      onChange={(e)=>setPassword(e.target.value)}
+                    />
+                  </FormControl>
 
-                    <FormControl
-                      style={{ margin: "15px 0" }}
-                      variant="standard"
-                      fullWidth
-                      required
+                  <FormControl style={{margin:"15px 0"}} variant="standard" fullWidth
+                      required>
+                    {/* <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel> */}
+                    <InputLabel id="demo-simple-select-standard-label">Role</InputLabel>
+                    <Select
+                    style={{margin:"15px 0",textAlign:"left"}}
+                    startAdornment={
+                      <InputAdornment style={{margin:"15px 10px 20px 0px"}} position="start">
+                        <PersonOutlineIcon fontSize='large' />
+                      </InputAdornment>
+                    }
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={role}
+                      onChange={handleChange}
+                      label="Role"
                     >
-                      {/* <InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel> */}
-                      <InputLabel id="demo-simple-select-standard-label">
-                        Role
-                      </InputLabel>
-                      <Select
-                        style={{ margin: "15px 0", textAlign: "left" }}
-                        startAdornment={
-                          <InputAdornment
-                            style={{ margin: "15px 10px 20px 0px" }}
-                            position="start"
-                          >
-                            <PersonOutlineIcon fontSize="large" />
-                          </InputAdornment>
-                        }
-                        labelId="demo-simple-select-standard-label"
-                        id="demo-simple-select-standard"
-                        value={role}
-                        onChange={handleChange}
-                        label="Role"
-                      >
-                        <MenuItem value={"reviewer"}>Reviewer</MenuItem>
-                        <MenuItem value={"programme_coordinator"}>
-                          Programme Coordinator
-                        </MenuItem>
-                        <MenuItem value={"iqau_director"}>
-                          IQAU Director
-                        </MenuItem>
-                        <MenuItem value={"cqa_director"}>CQA Director</MenuItem>
-                        <MenuItem value={"qac_director"}>QAC Director</MenuItem>
-                        <MenuItem value={"qac_officer"}>QAC Officer</MenuItem>
-                        <MenuItem value={"dean"}>Director/Dean</MenuItem>
-                        <MenuItem value={"vice_chancellor"}>
-                          Vice Chancellor
-                        </MenuItem>
-                      </Select>
-                    </FormControl>
-                    <FormGroup style={{ margin: "20px 0 0" }}>
-                      <FormControlLabel
-                        control={<Checkbox defaultChecked value={rememberMe} />}
-                        label="Remember me"
-                      />
-                    </FormGroup>
-                    {/* show errors */}
-                    <ColorButton type="submit" variant="contained">
-                      {loading ? "Signing In" : "Sign In"}
-                      {loading ? (
-                        <CircularProgress
-                          style={{ margin: "0 0 0 20px", color: "white" }}
-                          thickness={5}
-                          size={24}
-                        />
-                      ) : (
-                        ""
-                      )}
-                    </ColorButton>
-                  </form>
-                  <Typography style={{ marginTop: "15px" }}>
-                    <Link href="#">Forgot password ?</Link>
-                  </Typography>
-                  <Snackbar
-                    open={errorMsg == "" ? false : true}
-                    anchorOrigin={{ vertical: "top", horizontal: "center" }}
-                    onClose={() => setErrorMsg("")}
+                      <MenuItem value={"reviewer"}>Reviewer</MenuItem>
+                      <MenuItem value={"programme_coordinator"}>Programme Coordinator</MenuItem>
+                      <MenuItem value={"iqau_director"}>IQAU Director</MenuItem>
+                      <MenuItem value={"cqa_director"}>CQA Director</MenuItem>
+                      <MenuItem value={"qac_director"}>QAC Director</MenuItem>
+                      <MenuItem value={"qac_officer"}>QAC Officer</MenuItem>
+                      <MenuItem value={"dean"}>Director/Dean</MenuItem>
+                      <MenuItem value={"vice_chancellor"}>Vice Chancellor</MenuItem>
+                    </Select>
+
+                  </FormControl>
+                  <FormGroup style={{margin:"20px 0 0"}}>
+                    <FormControlLabel control={<Checkbox defaultChecked value={rememberMe}/>} label="Remember me" />
+                  </FormGroup>
+                  {/* show errors */}
+                  <ColorButton type='submit' variant="contained" fullWidth
                   >
+                    {loading? "Signing In" : "Sign In"}
+                    {loading ? <CircularProgress style={{margin:'0 0 0 20px',color:'white'}} thickness={5} size={24} /> : ''}
+                  </ColorButton>
+                </form>
+                <Typography style={{marginTop:"15px"}}>
+                  <Link href="#" >
+                    Forgot password ?
+                  </Link>
+                </Typography >
+                <Snackbar
+                    open={errorMsg =="" ? false : true}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                    onClose={() => setErrorMsg("")}
+                >
                     <Alert onClose={() => setErrorMsg("")} severity="error">
-                      {errorMsg}
+                        {errorMsg}
                     </Alert>
-                  </Snackbar>
-                  <Snackbar
+                </Snackbar>
+                <Snackbar
                     open={success}
                     autoHideDuration={1000}
                     onClose={() => setSuccess(false)}
-                    anchorOrigin={{ vertical: "top", horizontal: "center" }}
-                  >
+                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                >
                     <Alert onClose={() => setSuccess(false)} severity="success">
-                      Login Successfull!
+                        Login Successfull!
                     </Alert>
-                  </Snackbar>
-                </Grid>
-              </Box>
-            </Paper>
-          </Grid>
-        </Box>
-      </Grid>
-    </Box>
+                </Snackbar>
+            
+          </Box>
+        </Paper>
+
+      </Box>
+      <Box justifyContent={"center"} alignItems={"center"}>
+          <Typography  style={{color:"white"}} variant="h4" gutterBottom textAlign={"center"}>
+            Quality Assurance Framework for <br/> <span>Postgraduate</span> Programs
+          </Typography>
+          <img style={{height:'60vh',width:'60%',margin:"auto"}} src={"https://assets-global.website-files.com/5e8b3356a5a8f5321855bbe7/648c7d9b38164fd8fc587f8a_img-person-form.png"} alt="temporary" />
+      </Box>
+    </Container>
   );
 };
 
