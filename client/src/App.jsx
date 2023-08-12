fe727480ef21b708e3f48b383a3f8e407682f1ea
TharinduSampath2000 <stharindu444@gmail.com>
2023-08-12 22:56:37 +0530

    change: formatted code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,37 @@
-import {Route, Routes} from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Login from './components/Login';
-import Universities from './pages/QACOfficer/Universities';
-import ViewSer from './pages/Reviewer/ViewSer';
-import MainLayout from './components/MainLayout';
-import NotFound from './pages/NotFound';
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./pages/Login";
+import Universities from "./pages/QACOfficer/Universities";
+import ViewSer from "./pages/Reviewer/ViewSer";
+import MainLayout from "./components/MainLayout";
+import NotFound from "./pages/NotFound";
 import Authenticate from "./components/Authenticate";
 import Unauthorized from "./components/Unauthorized";
 import LoginPersist from "./components/LoginPersist.jsx";
-import PGPRApplication from './pages/Dean/PGPRApplication';
+import PGPRApplication from "./pages/Dean/PGPRApplication";
 import ResetInitialPassword from "./components/ResetInitialPassword.jsx";
-import AddPGProgramPage from './pages/CQAdirector/AddPGProgramPage';
-import AddAccounts from './pages/CQAdirector/AddAccounts';
-import EditPGProgram from './pages/CQAdirector/EditPGProgram';
-import SubmitIntent from './pages/DeanDirector/SubmitIntent';
-import SubmitConsent from './pages/DeanDirector/SubmitConsent';
-import SetDate from './pages/ReviewerChair/SetDate';
-import SetCriteria from './pages/ReviewerChair/SetCriteria';
-import PGAssignments from './pages/Reviewer/PGAssignments';
-import ConductDE from './pages/Reviewer/ConductDE';
-import ConductPE from './pages/Reviewer/ConductPE';
-import ViewUniversities from './pages/QACDirector/ViewUniversities'
-import ViewUniversity from './pages/QACDirector/ViewUniversity'
-import EditUniversity from './pages/QACDirector/EditUniversity';
-import AddUniversity from './pages/QACDirector/AddUniversity';
-import ViewReviewers from './pages/QACDirector/ViewReviewers'
-import CqaOffices from './pages/QACDirector/CqaOffices'
-import UpdateABC from './pages/Reviewer/UpdateABC';
-import Summary_details from './pages/Reviewer/Summary_details';
-import Standardwise_details from './pages/Reviewer/Standardwise_details';
-import AcceptAppointment from './pages/Reviewer/AcceptAppointment';
-import {DrawerStateProvider} from './contexts/DrawerStateProvider';
-import EvaluateDE from './pages/Reviewer/EvaluateDE';
+import AddPGProgramPage from "./pages/CQAdirector/AddPGProgramPage";
+import AddAccounts from "./pages/CQAdirector/AddAccounts";
+import EditPGProgram from "./pages/CQAdirector/EditPGProgram";
+import SubmitIntent from "./pages/DeanDirector/SubmitIntent";
+import SubmitConsent from "./pages/DeanDirector/SubmitConsent";
+import SetDate from "./pages/ReviewerChair/SetDate";
+import SetCriteria from "./pages/ReviewerChair/SetCriteria";
+import PGAssignments from "./pages/Reviewer/PGAssignments";
+import ConductDE from "./pages/Reviewer/ConductDE";
+import ConductPE from "./pages/Reviewer/ConductPE";
+import ViewUniversities from "./pages/QACDirector/ViewUniversities";
+import ViewUniversity from "./pages/QACDirector/ViewUniversity";
+import EditUniversity from "./pages/QACDirector/EditUniversity";
+import AddUniversity from "./pages/QACDirector/AddUniversity";
+import ViewReviewers from "./pages/QACDirector/ViewReviewers";
+import CqaOffices from "./pages/QACDirector/CqaOffices";
+import UpdateABC from "./pages/Reviewer/UpdateABC";
+import Summary_details from "./pages/Reviewer/Summary_details";
+import Standardwise_details from "./pages/Reviewer/Standardwise_details";
+import AcceptAppointment from "./pages/Reviewer/AcceptAppointment";
+import { DrawerStateProvider } from "./contexts/DrawerStateProvider";
+import EvaluateDE from "./pages/Reviewer/EvaluateDE";
 import "./App.css";
 
 /* 
@@ -42,125 +42,145 @@ import "./App.css";
   & instead do the changes into file already in there.
 */
 
-
 function App() {
-
   return (
-
     <Routes>
-        <Route path="/">
-            {/* guest routes */}
-            <Route path="unauthorized" element={<Unauthorized/>}></Route>
+      <Route path="/">
+        {/* guest routes */}
+        <Route path="unauthorized" element={<Unauthorized />}></Route>
 
-            <Route element={<LoginPersist/>}>
-              
-              {/* initial login password change component and the routes*/}
-             <Route path="initial-password-reset" element={<ResetInitialPassword />}/>
-             <Route path="accept-appointment" element={<AcceptAppointment />}/>
-             {/*note: note final*/}
-             <Route path="login" element={<Login/>}/>
-             <Route path="/" element={<Login/>}/>
-              
-             <Route element={<DrawerStateProvider><MainLayout/></DrawerStateProvider>}>
-                
-                {/* protected routes */}
+        <Route element={<LoginPersist />}>
+          {/* initial login password change component and the routes*/}
+          <Route
+            path="initial-password-reset"
+            element={<ResetInitialPassword />}
+          />
+          <Route path="accept-appointment" element={<AcceptAppointment />} />
+          {/*note: note final*/}
+          <Route path="login" element={<Login />} />
+          <Route path="/" element={<Login />} />
 
-                <Route element={<Authenticate allowedRoles={["qac_officer"]}/>}>
-                  <Route path="qac_officer/" >
-                      <Route path="" element={<Dashboard/>}/>
-                      <Route path="dashboard" element={<Dashboard/>}/>
-                      <Route path="universities" element={<Universities/>} />
-                  </Route>
-                </Route>
+          <Route
+            element={
+              <DrawerStateProvider>
+                <MainLayout />
+              </DrawerStateProvider>
+            }
+          >
+            {/* protected routes */}
 
-                <Route element={<Authenticate allowedRoles={["cqa_director"]}/>}>
-                  <Route path="cqa_director/" >
-                    <Route path="" element={<Dashboard/>}/>
-                    <Route path="dashboard" element={<Dashboard/>}/>
-                    <Route path="AddPGProgramPage" element={<AddPGProgramPage/>} />
-                    <Route path="AddAccounts" element={<AddAccounts/>} />
-                    <Route path="EditPGProgram" element={<EditPGProgram/>} />
-                  </Route>
-                </Route>
-
-                <Route element={<Authenticate allowedRoles={["qac_director"]}/>}>
-                  <Route path="qac_director/" >
-                      <Route path="" element={<Dashboard/>}/>
-                      <Route path="dashboard" element={<Dashboard/>}/>
-                      <Route path="universities/">
-                        <Route path="" element={<ViewUniversities/>} />
-                        <Route path="view/:id" element={<ViewUniversity/>} />
-                        <Route path="edit/:id" element={<EditUniversity/>} />
-                        <Route path="add" element={<AddUniversity/>} />
-                      </Route>
-                      <Route path="cqa_offices" element={<CqaOffices/>}/>
-                      <Route path="reviewers" element={<ViewReviewers/>}/>
-                  </Route>
-                </Route>
-
-                <Route element={<Authenticate allowedRoles={["reviewer"]}/>}>
-                  <Route path="reviewer/" >
-                      <Route path="" element={<Dashboard/>}/>
-                      <Route path="dashboard" element={<Dashboard/>}/>
-                      <Route path="PG_Assignments/">
-                        <Route path="" element={<PGAssignments/>} />
-                        <Route path="ViewSer/:uniId" element={<ViewSer/>} />
-                        <Route path="Conduct_DE/">
-                          <Route path=":uniId" element={<ConductDE/>} />
-                          <Route path=":uniId/:criteriaId" element={<EvaluateDE/>} />
-                          <Route path="UpdateABC/:uniId" element={<UpdateABC/>} />
-                          <Route path="Standardwise_details/:uniId" element={<Standardwise_details/>} />
-                          <Route path="Summary_details/:uniId" element={<Summary_details/>} />
-                        </Route>
-                        <Route path="Conduct_PE/" element={<ConductPE/>} >
-                          <Route path=":uniId" element={<ConductPE/>} />
-                          <Route path="Assigned_criteria/:uniId" element={<ConductPE/>} />
-                          <Route path="Assigned_criteria/:uniId/:criteriaId" element={<EvaluateDE/>} />
-                        </Route>
-                      </Route>
-                      <Route path="SetDate" element={<SetDate/>} />
-                      <Route path="SetCriteria" element={<SetCriteria/>} />
-                  </Route>
-                </Route>
-
-                {/* dean routes */}
-                <Route element={<Authenticate allowedRoles={["dean"]}/>}>
-                  <Route path="dean/">
-                    <Route path="" element={<Dashboard/>}/>
-                    <Route path="dashboard" element={<Dashboard/>}/>
-                    <Route path="SubmitIntent" element={<SubmitIntent/>} />
-                    <Route path="SubmitConsent" element={<SubmitConsent/>} />
-                  </Route>
-                </Route>
-
-                {/* iqau routes */}
-                <Route element={<Authenticate allowedRoles={["iqau_director"]}/>}>
-                  <Route path="iqau_director/">
-                    <Route path="" element={<Dashboard/>}/>
-                    <Route path="dashboard" element={<Dashboard/>}/>
-                  </Route>
-                </Route>
-
-                <Route element={<Authenticate allowedRoles={["programme_coordinator"]}/>}>
-                  <Route path="programme_coordinator/">
-                    <Route path="" element={<Dashboard/>}/>
-                    <Route path="dashboard" element={<Dashboard/>}/>
-                  </Route>
-                </Route>
-
-                <Route element={<Authenticate allowedRoles={["vice_chancellor"]}/>}>
-                  <Route path="vice_chancellor/">
-                    <Route path="" element={<Dashboard/>}/>
-                    <Route path="dashboard" element={<Dashboard/>}/>
-                  </Route>
-                </Route>
-
+            <Route element={<Authenticate allowedRoles={["qac_officer"]} />}>
+              <Route path="qac_officer/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="universities" element={<Universities />} />
               </Route>
-
             </Route>
-            {/* 404 page & UnAuth ... */}
-            <Route path="*" element={<NotFound/>}/>
+
+            <Route element={<Authenticate allowedRoles={["cqa_director"]} />}>
+              <Route path="cqa_director/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="AddPGProgramPage" element={<AddPGProgramPage />} />
+                <Route path="AddAccounts" element={<AddAccounts />} />
+                <Route path="EditPGProgram" element={<EditPGProgram />} />
+              </Route>
+            </Route>
+
+            <Route element={<Authenticate allowedRoles={["qac_director"]} />}>
+              <Route path="qac_director/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="universities/">
+                  <Route path="" element={<ViewUniversities />} />
+                  <Route path="view/:id" element={<ViewUniversity />} />
+                  <Route path="edit/:id" element={<EditUniversity />} />
+                  <Route path="add" element={<AddUniversity />} />
+                </Route>
+                <Route path="cqa_offices" element={<CqaOffices />} />
+                <Route path="reviewers" element={<ViewReviewers />} />
+              </Route>
+            </Route>
+
+            <Route element={<Authenticate allowedRoles={["reviewer"]} />}>
+              <Route path="reviewer/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="PG_Assignments/">
+                  <Route path="" element={<PGAssignments />} />
+                  <Route path="ViewSer/:uniId" element={<ViewSer />} />
+                  <Route path="Conduct_DE/">
+                    <Route path=":uniId" element={<ConductDE />} />
+                    <Route path=":uniId/:criteriaId" element={<EvaluateDE />} />
+                    <Route path="UpdateABC/:uniId" element={<UpdateABC />} />
+                    <Route
+                      path="Standardwise_details/:uniId"
+                      element={<Standardwise_details />}
+                    />
+                    <Route
+                      path="Summary_details/:uniId"
+                      element={<Summary_details />}
+                    />
+                  </Route>
+                  <Route path="Conduct_PE/">
+                    <Route path=":uniId" element={<ConductPE />} />
+                    <Route
+                      path="Assigned_criteria/:uniId"
+                      element={<ConductPE />}
+                    />
+                    <Route
+                      path="Assigned_criteria/:uniId/:criteriaId"
+                      element={<EvaluateDE />}
+                    />
+                  </Route>
+                </Route>
+                <Route path="SetDate" element={<SetDate />} />
+                <Route path="SetCriteria" element={<SetCriteria />} />
+              </Route>
+            </Route>
+
+            {/* dean routes */}
+            <Route element={<Authenticate allowedRoles={["dean"]} />}>
+              <Route path="dean/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="SubmitIntent" element={<SubmitIntent />} />
+                <Route path="SubmitConsent" element={<SubmitConsent />} />
+              </Route>
+            </Route>
+
+            {/* iqau routes */}
+            <Route element={<Authenticate allowedRoles={["iqau_director"]} />}>
+              <Route path="iqau_director/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+              </Route>
+            </Route>
+
+            <Route
+              element={
+                <Authenticate allowedRoles={["programme_coordinator"]} />
+              }
+            >
+              <Route path="programme_coordinator/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+              </Route>
+            </Route>
+
+            <Route
+              element={<Authenticate allowedRoles={["vice_chancellor"]} />}
+            >
+              <Route path="vice_chancellor/">
+                <Route path="" element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+              </Route>
+            </Route>
+          </Route>
         </Route>
+        {/* 404 page & UnAuth ... */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
