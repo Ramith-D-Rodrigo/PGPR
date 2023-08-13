dfd2fe379f08d846bbc699485217ec0572ded633
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-08-13 07:34:14 +0530

    Updated the validation rules for validating incoming data for creating a new review team

--- server/app/Http/Requests/V1/StoreReviewTeamRequest.php
+++ server/app/Http/Requests/V1/StoreReviewTeamRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\Rule;
 
 class StoreReviewTeamRequest extends FormRequest
 {
@@ -11,7 +13,8 @@ class StoreReviewTeamRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        // TODO: ONLY QAC OFFICER OR THE DIRECTOR CAN CREATE REVIEW TEAMS VALIDATION SHOULD BE IMPLEMENTED
+        return true;
     }
 
     /**
@@ -19,10 +22,39 @@ class StoreReviewTeamRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
+
+    /*
+     *
+     * TODO: NEED TO PROVIDE CUSTOM ERROR MESSAGES
+     * THIS SHOULD BE THE FORMAT OF THE INCOMING DATA
+     *  {
+     *      pgprId: 10,
+     *      reviewers:
+     *          [
+     *              {reviewId: 1, position: "MEMBER"},
+     *              {reviewId: 2, position: "CHAIR"},
+     *              {reviewId: 3, position: "MEMBER"}
+     *          ]
+     *  }
+     */
+
     public function rules(): array
     {
         return [
-            //
+            'pgpr_id' => 'required|exists:post_graduate_program_reviews,id',
+            'reviewers' => 'required|array|min:1|max:3',
+            'reviewers.*.reviewer_id' => 'required|exists:reviewers,id',
+            'reviewers.*.position' => ['required', Rule::in(['MEMBER', 'CHAIR'])],
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'pgpr_id' => $this->pgprId,
+            'reviewers' => Arr::map($this->reviewers, function ($reviewer) {
+                return ["reviewer_id" => $reviewer["reviewerId"] ?? null, "position" => $reviewer["position"] ?? null];
+            }),
+        ]);
     }
 }
