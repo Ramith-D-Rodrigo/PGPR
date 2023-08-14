9138484d649e124d250f911cc8c8255ab3eaadf7
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-08-14 19:57:32 +0530

    style:dashboard cards component

--- client/src/components/DashboardCard.jsx
+++ client/src/components/DashboardCard.jsx
@@ -12,8 +12,8 @@ let title = null,content=null,message=null
 const DashboardCard = ({title,content,message}) => {
   return (
     
-        <Card sx={{ width: "18rem", height: "10rem", textAlign: 'center', display:'flex', alignItems:'center', justifyContent:'center',backgroundColor:'#e6ffe6',
-                    borderBottom:"5px solid #99ff99" }}>
+        <Card sx={{ width: "18rem", height: "10rem", textAlign: 'center', display:'flex', alignItems:'center', justifyContent:'center',backgroundColor:'#D8E6FC',
+                    borderBottom:"4px solid #191970" }}>
             <CardContent>
                 <Typography gutterBottom variant='h6' component='div'>
                       {title}
