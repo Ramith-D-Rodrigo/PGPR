2be5fac2f1de3b4cfb2e2dd7b7371ac8450bbf98
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-08-16 20:23:48 +0530

    fix:show repetitive path as sidebar solved

--- client/src/components/BreadCrumbs.jsx
+++ client/src/components/BreadCrumbs.jsx
@@ -2,19 +2,31 @@ import { Breadcrumbs } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import UserNavigationsContext from '../contexts/UserNavigationsProvider';
+import useDrawerState from '../hooks/useDrawerState';
+import HomeIcon from '@mui/icons-material/Home';
 
 const BreadCrumbs = () => {
   
   const { userNavigations } = useContext(UserNavigationsContext);
+  const { drawerState } = useDrawerState();
 
   return (
+    <>
+    
     <Breadcrumbs aria-label='breadcrumb' sx={{flexGrow: 1}} separator='&#x27A4;'>
         {userNavigations.map((location, index) => {
+          if (drawerState.open && index===0) {
             return (
-                <Link key={index} to={location.link}>{location.name}</Link>
+              index==0 && <Link style={{display: 'flex', alignItems: 'center'}} to={location.link}><HomeIcon key={index} titleAccess={location.name} sx={{ mr: 1 }} fontSize="medium" /></Link>
             )
+          } else {
+              return (
+                <Link style={{color:"black",display: 'flex', alignItems: 'center'}} key={index} to={location.link}>{location.name}</Link>
+              )
+          }
         })}
     </Breadcrumbs>
+    </>
   )
 }
 
