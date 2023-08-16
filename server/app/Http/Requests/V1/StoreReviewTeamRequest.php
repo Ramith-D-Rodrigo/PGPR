f7b6de9d60e649959a2619fbfbe5813370540e54
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-08-16 18:06:34 +0530

    added the custom validation errors and improved validations

--- server/app/Http/Requests/V1/StoreReviewTeamRequest.php
+++ server/app/Http/Requests/V1/StoreReviewTeamRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\V1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Arr;
 use Illuminate\Validation\Rule;
@@ -20,12 +21,12 @@ class StoreReviewTeamRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
+    // protected $stopOnFirstFailure = true;
 
     /*
      *
-     * TODO: NEED TO PROVIDE CUSTOM ERROR MESSAGES
      * THIS SHOULD BE THE FORMAT OF THE INCOMING DATA
      *  {
      *      pgprId: 10,
@@ -43,8 +44,25 @@ class StoreReviewTeamRequest extends FormRequest
         return [
             'pgpr_id' => 'required|exists:post_graduate_program_reviews,id',
             'reviewers' => 'required|array|min:1|max:3',
-            'reviewers.*.reviewer_id' => 'required|exists:reviewers,id',
+            'reviewers.*.reviewer_id' => 'required|distinct|exists:reviewers,id',
             'reviewers.*.position' => ['required', Rule::in(['MEMBER', 'CHAIR'])],
+        ];
+    }
+
+    function messages(): array
+    {
+        return [
+            'pgpr_id.required' => 'We need to know the postgraduate program in order to assign a new review team.',
+            'pgpr_id.exists' => 'There is no such postgraduate program in our database.',
+            'reviewers.required' => 'Need to know the reviewers you are assigning.',
+            'reviewers.array' => 'Reviewers should be sent in an array.',
+            'reviewers.min' => 'Seems like there are no reviewers to add, please check and retry.',
+            'reviewers.max' => 'Sorry, you can only add at most 3 three reviewers per review team.',
+            'reviewers.*.reviewer_id.required' => 'You have not provided an id for the reviewer #:position, please check and get back.',
+            'reviewers.*.reviewer_id.distinct' => 'The #:position reviewer is duplicated, please check and get back.',
+            'reviewers.*.reviewer_id.exists' => 'The reviewer #:position that you provided do not exist in our database.',
+            'reviewers.*.position.required' => 'You have not provided the role of the reviewer #:position, please check and get back.',
+            'reviewers.*.position.rule' => 'The position that you provided for the reviewer #:position is not valid, it could only be either chair or member.',
         ];
     }
 
@@ -53,7 +71,13 @@ class StoreReviewTeamRequest extends FormRequest
         $this->merge([
             'pgpr_id' => $this->pgprId,
             'reviewers' => Arr::map($this->reviewers, function ($reviewer) {
-                return ["reviewer_id" => $reviewer["reviewerId"] ?? null, "position" => $reviewer["position"] ?? null];
+                return [
+                    "reviewer_id" => $reviewer["reviewerId"] ?? null,
+                    "position" => $reviewer["position"] ? $reviewer["position"] == 'member' ? 'MEMBER' :
+                        ($reviewer["position"] == 'chair' ? 'CHAIR' :
+                            $reviewer["position"]) :
+                        null
+                ];
             }),
         ]);
     }
