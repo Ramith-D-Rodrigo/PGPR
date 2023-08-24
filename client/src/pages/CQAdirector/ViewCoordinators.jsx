027e005c5561028bacedab0c66be7efc0ba4a9ff
fiya99banu <fiyaza.fb@gmail.com>
2023-08-24 10:57:22 +0530

    new commit

--- client/src/pages/CQAdirector/ViewCoordinators.jsx
+++ client/src/pages/CQAdirector/ViewCoordinators.jsx
@@ -1,26 +1,59 @@
 import React, { useState } from "react";
 import MainContent from "../../components/MainContent";
-import useSetUserNavigations from '../../hooks/useSetUserNavigations';
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar } from '@mui/material';
+import useSetUserNavigations from "../../hooks/useSetUserNavigations";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Avatar,
+} from "@mui/material";
 import { Link } from "react-router-dom";
-import { NavigateBefore, NavigateNext, SkipNext, SkipPrevious, Search } from '@mui/icons-material';
+import {
+  NavigateBefore,
+  NavigateNext,
+  SkipNext,
+  SkipPrevious,
+  Search,
+} from "@mui/icons-material";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
 
-const CustomTable = ({ tableData }) => {
+const CustomTable = ({ tableData, openEditDialog }) => {
   return (
     <div className="mt-6">
       <div className="overflow-x-auto">
-        <TableContainer component={Paper}>q
-
+        <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead style={{ backgroundColor: "#D8E6FC" }}>
               <TableRow>
-                <TableCell align="center"><b>Profile Photo</b></TableCell>
-                <TableCell align="center"><b>C-ID</b></TableCell>
-                <TableCell align="center"><b>Name</b></TableCell>
-                <TableCell align="center"><b>Faculty</b></TableCell>
-                <TableCell align="center"><b>Status</b></TableCell>
-                <TableCell align="center"><b>No. of PG Programs</b></TableCell>
-                <TableCell align="center"><b>Actions</b></TableCell>
+                <TableCell align="center">
+                  <b>Profile Photo</b>
+                </TableCell>
+                <TableCell align="center">
+                  <b>C-ID</b>
+                </TableCell>
+                <TableCell align="center">
+                  <b>Name</b>
+                </TableCell>
+                <TableCell align="center">
+                  <b>Faculty</b>
+                </TableCell>
+                <TableCell align="center">
+                  <b>Status</b>
+                </TableCell>
+                <TableCell align="center">
+                  <b>No. of PG Programs</b>
+                </TableCell>
+                <TableCell align="center">
+                  <b>Actions</b>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -51,11 +84,11 @@ const CustomTable = ({ tableData }) => {
                       variant="contained"
                       color="primary"
                       size="small"
-                      component={Link}
-                      to={"/edit/" + row.cid}
+                      onClick={() => openEditDialog(row)}
                     >
                       Edit
                     </Button>
+
                     {/* Removed the onClick event for Delete button */}
                   </TableCell>
                 </TableRow>
@@ -69,6 +102,62 @@ const CustomTable = ({ tableData }) => {
 };
 
 const Coordinators = () => {
+
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  
+  const [tableData, setTableData] = useState([ {
+    cid: "C-100",
+    name: "Dr. Manju",
+    faculty: "Science",
+    status: "Approved",
+    pgCount: 3,
+    profilePhoto: "https://randomuser.me/api/portraits/men/1.jpg", // Professional profile photo of a gentleman
+  },
+  {
+    cid: "C-101",
+    name: "Dr. Pasindu",
+    faculty: "Arts",
+    status: "Confirmed",
+    pgCount: 2,
+    profilePhoto: "https://randomuser.me/api/portraits/men/2.jpg", // Professional profile photo of a gentleman
+  },
+  {
+    cid: "C-102",
+    name: "Dr. Thilini",
+    faculty: "Management",
+    status: "Canceled",
+    pgCount: 5,
+    profilePhoto: "https://randomuser.me/api/portraits/women/1.jpg", // Professional profile photo of a lady
+  },
+]);
+
+const [selectedCoordinatorForEdit, setSelectedCoordinatorForEdit] = useState(tableData[0]);
+
+  // Define openEditPopup function within the Coordinators component
+  const handleOpenEditDialog = (coordinator) => {
+    setSelectedCoordinatorForEdit(coordinator);
+    setIsEditDialogOpen(true);
+  };
+
+  const handleSaveEdit = () => {
+    if (selectedCoordinatorForEdit) {
+      // Update the tableData with the edited coordinator details
+      const updatedTableData = tableData.map((coordinator) =>
+        coordinator.cid === selectedCoordinatorForEdit.cid
+          ? selectedCoordinatorForEdit
+          : coordinator
+      );
+  
+      setTableData(updatedTableData);
+      handleCloseEditDialog();
+    }
+  };
+
+  const handleCloseEditDialog = () => {
+    setSelectedCoordinatorForEdit(null);
+    setIsEditDialogOpen(false);
+  };
+
   useSetUserNavigations([
     {
       name: "View Program Coordinators",
@@ -84,33 +173,8 @@ const Coordinators = () => {
     setSearchedDirector(directorName);
   };
 
-  // Hardcoded data for the table
-  const tableData = [
-    {
-      cid: "C-100",
-      name: "Dr. Manju",
-      faculty: "Science",
-      status: "Approved",
-      pgCount: 3,
-      profilePhoto: "https://randomuser.me/api/portraits/men/1.jpg", // Professional profile photo of a gentleman
-    },
-    {
-      cid: "C-101",
-      name: "Dr. Pasindu",
-      faculty: "Arts",
-      status: "Confirmed",
-      pgCount: 2,
-      profilePhoto: "https://randomuser.me/api/portraits/men/2.jpg", // Professional profile photo of a gentleman
-    },
-    {
-      cid: "C-102",
-      name: "Dr. Thilini",
-      faculty: "Management",
-      status: "Canceled",
-      pgCount: 5,
-      profilePhoto: "https://randomuser.me/api/portraits/women/1.jpg", // Professional profile photo of a lady
-    },
-  ];
+
+   
 
   const handleNavigateBefore = () => {
     // Handle the navigate before action here
@@ -133,63 +197,182 @@ const Coordinators = () => {
   };
 
   return (
-      <div className="max-w-6xl mx-auto p-6 bg-white rounded-md mt-6">
-        <h2 className="text-2xl font-bold text-center">View Program Coordinators/Dean (IQAU Director) </h2>
-        <hr className="border-t-2 border-black my-4 opacity-50" />
-        <div className="grid grid-cols-2 gap-4">
-          <div className="flex flex-col">
-            <label htmlFor="university" className="block font-medium text-gray-700">
-              University Name:
-            </label>
-            <input
-              type="text"
-              id="university"
-              className="form-input border border-black p-1"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="director" className="block font-medium text-gray-700">
-              IQAU Director:
-            </label>
-            <input
-              type="text"
-              id="director"
-              className="form-input border border-black p-1"
-            />
-          </div>
+    <div className="max-w-6xl mx-auto p-6 bg-white rounded-md mt-6">
+      <h2 className="text-2xl font-bold text-center">
+        View Program Coordinators/Dean (IQAU Director){" "}
+      </h2>
+      <hr className="border-t-2 border-black my-4 opacity-50" />
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col">
+          <label
+            htmlFor="university"
+            className="block font-medium text-gray-700"
+          >
+            University Name:
+          </label>
+          <input
+            type="text"
+            id="university"
+            className="form-input border border-black p-1"
+          />
         </div>
-        <div className="flex justify-center mt-4">
-          <Button variant="contained" color="primary" onClick={() => handleSearch()}>
-            <Search />
-            Search
-          </Button>
-        </div>
-        <div className="flex justify-center mt-4">
-          <Button variant="contained" color="primary">
-            Show Current Coordinators
-          </Button>
-          <Button variant="contained" color="primary" style={{ marginLeft: "8px" }}>
-            Show Current Dean/Director
-          </Button>
-        </div>
-
-        <CustomTable tableData={tableData} />
-
-        <div className="flex justify-end mt-4"> {/* Align buttons to the right */}
-          <NavigateBefore color="primary" fontSize="large" />
-          <SkipPrevious color="primary" fontSize="large" />
-          <SkipNext color="primary" fontSize="large" />
-          <NavigateNext color="primary" fontSize="large" />
-        </div>
-        <div className="flex justify-start mt-4"> {/* Align buttons to the left */}
-          <Button variant="contained" color="primary" style={{ marginRight: "8px" }}>
-            Select Coordinator/Dean for PGPR
-          </Button>
-          <Button variant="contained" color="primary" style={{ marginRight: "8px" }}>
-            New Coordinator/Dean
-          </Button>
+        <div className="flex flex-col">
+          <label htmlFor="director" className="block font-medium text-gray-700">
+            IQAU Director:
+          </label>
+          <input
+            type="text"
+            id="director"
+            className="form-input border border-black p-1"
+          />
         </div>
       </div>
+      <div className="flex justify-center mt-4">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleSearch()}
+        >
+          <Search />
+          Search
+        </Button>
+      </div>
+      <div className="flex justify-center mt-4">
+        <Button variant="contained" color="primary">
+          Show Current Coordinators
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginLeft: "8px" }}
+        >
+          Show Current Dean/Director
+        </Button>
+      </div>
+
+      <CustomTable
+        tableData={tableData}
+        openEditDialog={handleOpenEditDialog}
+      />
+
+      <Dialog open={isEditDialogOpen} onClose={handleCloseEditDialog}>
+        <DialogTitle>Edit Coordinator</DialogTitle>
+        <DialogContent>
+        <form className="dialog-form">
+        <div className="form-group">
+            <label htmlFor="cid">C-ID:</label>
+            <input
+              type="text"
+              id="cid"
+              value={selectedCoordinatorForEdit.cid}
+              // Add an onChange handler to update selectedCoordinatorForEdit.cid
+              onChange={(e) =>
+                setSelectedCoordinatorForEdit((prevState) => ({
+                  ...prevState,
+                  cid: e.target.value,
+                }))
+              }
+            />
+            </div>
+
+            <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={selectedCoordinatorForEdit.name}
+              // Add an onChange handler to update selectedCoordinatorForEdit.name
+              onChange={(e) =>
+                setSelectedCoordinatorForEdit((prevState) => ({
+                  ...prevState,
+                  name: e.target.value,
+                }))
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="faculty">Faculty:</label>
+            <input
+              type="text"
+              id="faculty"
+              value={selectedCoordinatorForEdit.faculty}
+              // Add an onChange handler to update selectedCoordinatorForEdit.faculty
+              onChange={(e) =>
+                setSelectedCoordinatorForEdit((prevState) => ({
+                  ...prevState,
+                  faculty: e.target.value,
+                }))
+              }
+            />
+            </div>
+
+            <div className="form-group">
+            <label htmlFor="status">Status:</label>
+            <input
+              type="text"
+              id="status"
+              value={selectedCoordinatorForEdit.status}
+              // Add an onChange handler to update selectedCoordinatorForEdit.pgCount
+              onChange={(e) =>
+                setSelectedCoordinatorForEdit((prevState) => ({
+                  ...prevState,
+                  status: e.target.value,
+                }))
+              }
+            />
+          </div>
+
+            <div className="form-group">
+            <label htmlFor="pgCount">No. of PG Programs:</label>
+            <input
+              type="number"
+              id="pgCount"
+              value={selectedCoordinatorForEdit.pgCount}
+              // Add an onChange handler to update selectedCoordinatorForEdit.pgCount
+              onChange={(e) =>
+                setSelectedCoordinatorForEdit((prevState) => ({
+                  ...prevState,
+                  pgCount: e.target.value,
+                }))
+              }
+            />
+            </div>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditDialog}>Cancel</Button>
+          <Button onClick={handleSaveEdit}>Save</Button>
+        </DialogActions>
+      </Dialog>
+
+      <div className="flex justify-end mt-4">
+        {" "}
+        {/* Align buttons to the right */}
+        <NavigateBefore color="primary" fontSize="large" />
+        <SkipPrevious color="primary" fontSize="large" />
+        <SkipNext color="primary" fontSize="large" />
+        <NavigateNext color="primary" fontSize="large" />
+      </div>
+      <div className="flex justify-start mt-4">
+        {" "}
+        {/* Align buttons to the left */}
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginRight: "8px" }}
+        >
+          Select Coordinator/Dean for PGPR
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginRight: "8px" }}
+        >
+          New Coordinator/Dean
+        </Button>
+      </div>
+    </div>
   );
 };
 
