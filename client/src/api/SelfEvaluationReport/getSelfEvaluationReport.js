3d8cd41603cf81e62e580a26d50847e4b90b0524
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-08-30 12:59:30 +0530

    self evaluation report api callers (missed one lol)

---
+++ client/src/api/SelfEvaluationReport/getSelfEvaluationReport.js
@@ -0,0 +1,14 @@
+import { SERVER_URL, SERVER_API_VERSION } from "../../assets/constants";
+import axios from "../api.js";
+
+//role of this function: get self evaluation report by id
+
+//url is : SERVER_URL + SERVER_API_VERSION + 'selfEvaluationReports/{selfEvaluationReportId}'
+
+const getSelfEvaluationReport = async (selfEvaluationReportId) => {
+    let URL = SERVER_URL + SERVER_API_VERSION + 'selfEvaluationReports/' + selfEvaluationReportId;
+
+    return await axios.get(URL);
+}
+
+export default getSelfEvaluationReport;
