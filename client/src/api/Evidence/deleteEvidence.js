d6776bc668e2a9504b4c68454ca0f4724b299507
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-08-30 13:01:25 +0530

    evidence api callers

---
+++ client/src/api/Evidence/deleteEvidence.js
@@ -0,0 +1,14 @@
+import { SERVER_URL, SERVER_API_VERSION } from "../../assets/constants";
+import axios from "../api.js";
+
+//role of this function: delete an evidence
+
+//url is : SERVER_URL + SERVER_API_VERSION + 'evidences'
+let URL = SERVER_URL + SERVER_API_VERSION + 'evidences/'; // endpoint should be -> evidences/{evidenceId}
+
+const deleteEvidence = async (evidenceId) => {
+    URL = URL + evidenceId;
+    return await axios.delete(URL);
+}
+
+export default deleteEvidence;
