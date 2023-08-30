b9a7c63d188d88bdbfa40f1ba38abc4b6eb5b952
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-08-30 12:58:27 +0530

    deprecated functions

--- client/src/api/Dean/handlePGPRApplicationCreation.js
+++ client/src/api/Dean/handlePGPRApplicationCreation.js
@@ -1,6 +1,8 @@
 import { SERVER_API_VERSION, SERVER_URL } from "../../assets/constants.js";
 import axios from "../api.js";
 
+//DEPRECATED FUNCTION
+
 const URL = SERVER_URL + SERVER_API_VERSION + 'pgprApplications/';
 
 const handlePGPRApplicationCreation = (formData) => {
