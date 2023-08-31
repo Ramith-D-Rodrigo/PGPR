88e67ce425c9ca79688b13a923fd3b6f4b8fa22b
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-08-31 08:48:41 +0530

    change: add accounts in CQA director

--- client/src/pages/CQAdirector/AddAccounts.jsx
+++ client/src/pages/CQAdirector/AddAccounts.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
-import Form from '../../components/Form';
-import MainContent from '../../components/MainContent';
 import useSetUserNavigations from '../../hooks/useSetUserNavigations';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AddDean from './AddDean';
+import AddProgrammeCoordinator from './AddProgrammeCoordinator';
+import AddIQAUDiretor from './AddIQAUDiretor';
 
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const AddAccounts = () => {
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   useSetUserNavigations(
     [
@@ -28,27 +72,24 @@ const AddAccounts = () => {
   };
 
   return (
-   
-      
-        <Form
-          topic="Create Account for Program Coordinator/Director/Dean"
-          fields={[
-            { name: 'name', label: 'Name', type: 'text' },
-            { name: 'title', label: 'Title', type: 'select', options: ['Mr', 'Dr', 'Prof', 'Mrs', 'Ms'] },
-            { name: 'cId', label: 'C-ID', type: 'text' },
-            { name: 'position', label: 'Position', type: 'text' },
-            { name: 'faculty', label: 'Faculty/Institute Name', type: 'text' },
-            { name: 'department', label: 'Department', type: 'text' },
-            { name: 'qualification', label: 'Qualification', type: 'text' },
-            { name: 'date', label: 'Date', type: 'date' },
-            { name: 'email', label: 'Email Address', type: 'email' },
-            { name: 'status', label: 'Status', type: 'select', options: ['Confirmed', 'Active', 'Accepted', 'Cancelled'] },
-          ]}
-          cancelButtonText="Cancel"
-          submitButtonText="Create Account"
-          onCancel={handleCancel}
-          onSubmit={handleSubmit}
-        />
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider',marginBottom: 2, }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Add a Dean/Director" {...a11yProps(0)} />
+          <Tab label="Add a Programme Coordinator" {...a11yProps(1)} />
+          <Tab label="Add a IQAU director" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <AddDean/>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <AddProgrammeCoordinator/>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <AddIQAUDiretor/>
+      </CustomTabPanel>
+  </Box>
  
   );
 };
