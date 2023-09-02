dadcf10fe8136571a60d42b5660ad9cc6c6e4924
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-09-02 22:05:04 +0530

    added return types and auto reformatting

--- server/app/Models/DeskEvaluation.php
+++ server/app/Models/DeskEvaluation.php
@@ -7,23 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class DeskEvaluation extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  // Desk Evaluation belongs to a PostGraduateReviewProgram
-  public function postGraduateProgramReview()
-  {
-    return $this->belongsTo(PostGraduateProgramReview::class, 'pgpr_id', 'id');
-  }
+    // Desk Evaluation belongs to a PostGraduateReviewProgram
+    public function postGraduateProgramReview(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(PostGraduateProgramReview::class, 'pgpr_id', 'id');
+    }
 
-  // desk evaluation has 7(many) standards and associated scores
-  public function standards()
-  {
-      return $this->belongsToMany(
-          Standard::class,
-          'desk_evaluation_score',
-          'desk_evaluation_id',
-          'standard_id' // the column in the relating model
-      )->withPivot('de_score');
-  }
-
+    // desk evaluation has 7(many) standards and associated scores
+    public function standards(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(
+            Standard::class,
+            'desk_evaluation_score',
+            'desk_evaluation_id',
+            'standard_id' // the column in the relating model
+        )->withPivot('de_score');
+    }
 }
