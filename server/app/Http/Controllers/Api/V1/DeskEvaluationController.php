73e125da2aa32a486d3dfb666793d8e24214c188
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-09-02 22:03:31 +0530

    a complete (without the destroy method) untested class for desk evaluations

--- server/app/Http/Controllers/Api/V1/DeskEvaluationController.php
+++ server/app/Http/Controllers/Api/V1/DeskEvaluationController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\V1\StoreDeskEvaluationRequest;
 use App\Http\Requests\V1\UpdateDeskEvaluationRequest;
+use App\Http\Resources\V1\DeskEvaluationCollection;
+use App\Http\Resources\V1\DeskEvaluationResource;
 use App\Models\DeskEvaluation;
 use App\Http\Controllers\Controller;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class DeskEvaluationController extends Controller
@@ -18,46 +21,69 @@ class DeskEvaluationController extends Controller
     public function index(Request $request)
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return new DeskEvaluationCollection(DeskEvaluation::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreDeskEvaluationRequest $request)
+    public function store(StoreDeskEvaluationRequest $request): DeskEvaluationResource|\Illuminate\Http\JsonResponse
     {
         //
+        // create the record
+        try {
+            $validated = $request->validated();
+
+            $deskEvaluation = new DeskEvaluation();
+            $deskEvaluation->pgpr_id = $validated['pgpr_id'];
+            $deskEvaluation->start_date = $validated['start_date'];
+            $deskEvaluation->end_date = $validated['end_date'];
+            $deskEvaluation->status = 'ONGOING';
+            $deskEvaluation->save();
+
+            return new DeskEvaluationResource($deskEvaluation);
+        } catch (Exception $exception) {
+            return response()->json(['message' => 'We have encountered an error, try again in a few moments please'], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(DeskEvaluation $deskEvaluation)
+    public function show(string $id): DeskEvaluationResource|\Illuminate\Http\JsonResponse
     {
         //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(DeskEvaluation $deskEvaluation)
-    {
-        //
+        try {
+            return new DeskEvaluationResource(DeskEvaluation::findOrFail($id));
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => 'The desk evaluation id you mentioned could not be found please try again after making amends'], 422);
+        } catch (Exception $exception) {
+            return response()->json(['message' => 'We have encountered an error, try again in a few moments please'], 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateDeskEvaluationRequest $request, DeskEvaluation $deskEvaluation)
+    public function update(UpdateDeskEvaluationRequest $request, string $id): DeskEvaluationResource|\Illuminate\Http\JsonResponse
     {
         //
+        try {
+            $validated = $request->validated();
+            $deskEvaluation = DeskEvaluation::findOrFail($id);
+
+            $deskEvaluation->update(
+                $validated
+            );
+
+            $deskEvaluation->save();
+
+            return new DeskEvaluationResource($deskEvaluation);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['message' => 'The desk evaluation id you mentioned could not be found please try again after making amends'], 422);
+        } catch (Exception $exception) {
+            return response()->json(['message' => 'We have encountered an error, try again in a few moments please'], 500);
+        }
     }
 
     /**
