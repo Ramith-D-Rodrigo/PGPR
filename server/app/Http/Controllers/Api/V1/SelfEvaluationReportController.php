3c8bfad53955eee3e7db8833704ae321aec6192e
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-09-02 12:22:38 +0530

    removed the create and edit functions not needed for an API

--- server/app/Http/Controllers/Api/V1/SelfEvaluationReportController.php
+++ server/app/Http/Controllers/Api/V1/SelfEvaluationReportController.php
@@ -33,14 +33,6 @@ class SelfEvaluationReportController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreSelfEvaluationReportRequest $request)
@@ -104,14 +96,6 @@ class SelfEvaluationReportController extends Controller
         ]);
 
         return new SelfEvaluationReportResource($selfEvaluationReport);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(SelfEvaluationReport $selfEvaluationReport)
-    {
-        //
     }
 
     /**
