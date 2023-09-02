93e7255df434a1dc58d421245a9a67bec3f3b531
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-09-02 11:24:29 +0530

    added the reviewTeam() fucntion to get the review team relationship and reformatted the file

--- server/app/Models/PostGraduateProgramReview.php
+++ server/app/Models/PostGraduateProgramReview.php
@@ -4,72 +4,74 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use function Symfony\Component\String\u;
 
 class PostGraduateProgramReview extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  protected $fillable = [
-    'post_graduate_program_id',
-    'qac_dir_id',
-    'status_of_pgpr',
-    'payment_voucher',
-    'grouped_with',
-    'action_plan',
-    'preliminary_report',
-    'pgpr_application_id'
-  ];
+    protected $fillable = [
+        'post_graduate_program_id',
+        'qac_dir_id',
+        'status_of_pgpr',
+        'payment_voucher',
+        'grouped_with',
+        'action_plan',
+        'preliminary_report',
+        'pgpr_application_id'
+    ];
 
-/*  // a post graduate review program has one desk evaluation
-  public function deskEvaluations()
-  {
-    return $this->hasOne(DeskEvaluation::class, 'de_id', 'id');
-  }
+    // a post graduate review program has one desk evaluation
+    public function deskEvaluations(): HasOne
+    {
+        return $this->hasOne(DeskEvaluation::class, 'pgpr_id', );
+    }
 
-  // a post graduate review program has one proper evaluation
-  public function poperEvaluations()
-  {
-    return $this->hasOne(ProperEvaluation::class, 'pe_id', 'id');
-  }*/
+    // a post graduate review program has one proper evaluation
+    public function properEvaluations(): HasOne
+    {
+        return $this->hasOne(ProperEvaluation::class, 'pgpr_id');
+    }
 
-  // pgpr sentIntentLetter relation
-  public function deans()
-  {
-      return $this->belongsTo(Dean::class, 'dean_id', 'id');
-  }
+    // pgpr sentIntentLetter relation
+    public function postGraduateProgramReviewApplication(): BelongsTo
+    {
+        return $this->belongsTo(PostGraduateProgramReviewApplication::class, 'pgpr_application_id', 'id');
+    }
 
-/*  // pgpr has a review team associated with itself
-  public function reviewTeams()
-  {
-      return $this->hasOne(PostGraduateProgramReview::class, 'review_team_id');
-  }*/
+    public function postGraduateProgram(): BelongsTo
+    {
+        return $this->belongsTo(PostGraduateProgram::class, 'post_graduate_program_id', 'id');
+    }
 
-  public function postGraduateProgram()
-  {
-      return $this->belongsTo(PostGraduateProgram::class, 'post_graduate_program_id', 'id');
-  }
-
-  //pgpr has a self evaluation report
-    public function selfEvaluationReport()
+    //pgpr has a self-evaluation report
+    public function selfEvaluationReport(): HasOne
     {
         return $this->hasOne(SelfEvaluationReport::class);
     }
 
-  // every pgpr has a final report
-  public function finalReports()
-  {
-      return $this->hasOne(FinalReport::class, 'final_report_id');
-  }
+    // every pgpr has a final report
+    public function finalReports(): HasOne
+    {
+        return $this->hasOne(FinalReport::class, 'final_report_id');
+    }
 
-  // pgprs may be rejected by the QACDiretor
-  public function qualityAssuranceCouncilDirectors()
-  {
-      return $this->belongsTo(QualityAssuranceCouncilDirector::class, 'qac_dir_id', 'id');
-  }
+    // pgprs may be rejected by the QACDiretor
+    public function qualityAssuranceCouncilDirectors(): BelongsTo
+    {
+        return $this->belongsTo(QualityAssuranceCouncilDirector::class, 'qac_dir_id', 'id');
+    }
 
-  //post graduate program review has many post graduate program review applications
-    public function postGraduateProgramReviewApplications(){
+    //post graduate program review has many post graduate program review applications
+    public function postGraduateProgramReviewApplications(): BelongsTo
+    {
         return $this->belongsTo(PostGraduateProgramReviewApplication::class, 'pgpr_application_id', 'id');
+    }
+
+    public function reviewTeam(): HasOne
+    {
+        return $this->hasOne(ReviewTeam::class, 'pgpr_id');
     }
 }
