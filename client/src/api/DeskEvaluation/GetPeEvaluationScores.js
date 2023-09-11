9bbb0d266f64f30efa7e43b3ab2b2a8532d2ba05
Charith Jayarangana <cj.ptsz@yahoo.com>
2023-09-11 13:07:03 +0530

    create some API calls for backend

---
+++ client/src/api/DeskEvaluation/GetPeEvaluationScores.js
@@ -0,0 +1,17 @@
+import { SERVER_URL, SERVER_API_VERSION } from "../../assets/constants";
+import axios from "../api.js";
+
+//role of this function: Display Given PE's Scores if marked
+//reviewer display the standards of a criteria with scores and comments +> URL params properEvaluation=10&criteria=12
+
+//url is : SERVER_URL + SERVER_API_VERSION + '/reviewer/proper-evaluation/criteria/display-remarks-scores'
+
+const URL = SERVER_URL + SERVER_API_VERSION + '/reviewer/proper-evaluation/criteria/display-remarks-scores';
+
+const GetPeEvaluationScores = async (postGraduateProgramData) => {
+
+    await axios.get("/sanctum/csrf-cookie"); //csrf-cookie is required for post requests
+    return await axios.post(URL, postGraduateProgramData);
+}
+
+export default GetPeEvaluationScores;
