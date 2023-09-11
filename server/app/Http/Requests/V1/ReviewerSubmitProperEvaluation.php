add409cb7220a2146854bf2bdc9ab30b5bffec16
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-09-11 10:26:47 +0530

    new request class for validating proper evaluation submit data

--- server/app/Http/Requests/V1/ReviewerSubmitProperEvaluation.php
+++ server/app/Http/Requests/V1/ReviewerSubmitProperEvaluation.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\V1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ReviewerSubmitProperEvaluation extends FormRequest
@@ -11,18 +12,37 @@ class ReviewerSubmitProperEvaluation extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
+     *  POST request +>
+     *               properEvaluation=10
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'proper_evaluation_id' => 'required|exists:proper_evaluations,id'
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge(
+            [
+                'proper_evaluation_id' => $this->properEvaluation
+            ]
+        );
+    }
+
+    public function messages(): array
+    {
+        return [
+            'proper_evaluation_id.required' => 'The proper evaluation id is required',
+            'proper_evaluation_id.exists' => 'The proper evaluation does not exist',
         ];
     }
 }
