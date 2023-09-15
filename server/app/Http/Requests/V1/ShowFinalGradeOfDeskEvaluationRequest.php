60b8188929f4a6d5d65e5d2bded42ba8375f16cf
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-09-15 20:24:03 +0530

    added validation rules for ShowFinalGradeOfDeskEvaluationRequest

--- server/app/Http/Requests/V1/ShowFinalGradeOfDeskEvaluationRequest.php
+++ server/app/Http/Requests/V1/ShowFinalGradeOfDeskEvaluationRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\V1;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ShowFinalGradeOfDeskEvaluationRequest extends FormRequest
 {
@@ -11,18 +13,41 @@ class ShowFinalGradeOfDeskEvaluationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
+     *
+     * GET request +>
+     *              {
+     *                  pgpr: 10
+     *              }
+     *
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'pgpr_id' => ['required', Rule::exists('post_graduate_program_reviews', 'id')->where(function ($query) {
+                $query->whereNotIn('status_of_pgpr', ['PLANNING', 'SUBMITTED', 'SUSPENDED', 'DE', 'PE']);
+            })]
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'pgpr_id' => $this->pgpr
+        ]);
+    }
+
+    public function messages(): array
+    {
+        return [
+            'pgpr_id.required' => 'The post graduate program id is required.',
+            'pgpr_id.exists' => 'There is no such a post graduate review program in our system or, the review process has not yet reach a level the grades can be viewed.',
         ];
     }
 }
