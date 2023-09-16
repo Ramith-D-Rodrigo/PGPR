fee6c5aaf72c0d6f0d88f721f4ac61cbf94ef8f5
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-09-16 12:27:58 +0530

    popup for viewing the application

--- client/src/pages/ProgrammeCoordinator/PGPRs.jsx
+++ client/src/pages/ProgrammeCoordinator/PGPRs.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { Table, TableRow, TableCell, TableContainer, TableHead, TableBody } from '@mui/material';
+import { Table, TableRow, TableCell, TableContainer, TableHead, TableBody, Modal, Typography, List, ListItem } from '@mui/material';
 import useAuth from '../../hooks/useAuth';
 import getCoordinatorPGP from '../../api/ProgrammeCoordinator/getCoordinatorPGP';
 import getPostGraduatePRogramPGPRs from '../../api/PostGraduateProgram/getPostGraduatePRogramPGPRs';
 import { Button, ButtonGroup } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import { Style } from '@mui/icons-material';
+import getPGPRApplication from '../../api/PostGraduateProgramApplication/getPGPRApplication';
+import { SERVER_URL } from '../../assets/constants';
 
 const PGPRs = () => {
 
@@ -14,13 +18,13 @@ const PGPRs = () => {
 
   React.useEffect(() => {
     const getData = async () => {
-      try{
-        const pgpResponse = await getCoordinatorPGP(auth.id); 
+      try {
+        const pgpResponse = await getCoordinatorPGP(auth.id);
 
         if (pgpResponse && pgpResponse.status === 200) {
           console.log(pgpResponse.data.data);
-          const pgprResponse = await getPostGraduatePRogramPGPRs(pgpResponse.data.data.id, {includeSelfEvaluationReport: true});
-  
+          const pgprResponse = await getPostGraduatePRogramPGPRs(pgpResponse.data.data.id, { includeSelfEvaluationReport: true });
+
           if (pgprResponse && pgprResponse.status === 200) {
             console.log(pgprResponse.data.data);
 
@@ -31,7 +35,7 @@ const PGPRs = () => {
           }
         }
       }
-      catch(error){
+      catch (error) {
         console.log(error);
       }
     }
@@ -39,8 +43,124 @@ const PGPRs = () => {
     getData();
   }, [auth.id]);
 
+  const [viewingApplication, setViewingApplication] = React.useState(null);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const openApplicationPopUp = () => {
+    setOpen(true);
+  };
+
+  const closeApplicationPopUp = () => {
+    setOpen(false);
+    setViewingApplication(null);
+  };
+
+  const [viewingApplicationData, setViewingApplicationData] = React.useState(null);
+
+  useEffect(() => {
+    const getApplicationData = async () => {
+      if (!viewingApplication) {
+        return;
+      }
+      try {
+        const response = await getPGPRApplication(viewingApplication);
+
+        if (response && response.status === 200) {
+          console.log(response.data.data);
+          setViewingApplicationData(response.data.data);
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getApplicationData();
+  }, [viewingApplication]);
+
+
+
+
+  //create a simple modal using material ui
+  const ApplicationPopUp = () => {
+    return (
+      <Modal
+        open={open}
+        onClose={closeApplicationPopUp}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Post Graduate Programme Review Application
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <Typography>
+              Application ID: {viewingApplicationData?.id}
+            </Typography>
+            <Typography>
+              Application Status: {viewingApplicationData?.status.charAt(0).toUpperCase() + viewingApplicationData?.status.slice(1)}
+            </Typography>
+            <Typography>
+              Request Date: {viewingApplicationData?.requestDate}
+            </Typography>
+            <Typography>
+              Application Date: {viewingApplicationData?.applicationDate}
+            </Typography>
+            <Typography>
+              Years:
+              <List>
+                <ListItem>
+                  <Typography>
+                    Year 1: {viewingApplicationData?.year1}
+                  </Typography>
+                  <Typography>
+                    Year 2: {viewingApplicationData?.year2}
+                  </Typography>
+                  <Typography>
+                    Year 3: {viewingApplicationData?.year3}
+                  </Typography>
+                  <Typography>
+                    Year 4: {viewingApplicationData?.year4}
+                  </Typography>
+                  <Typography>
+                    Year 5: {viewingApplicationData?.year5}
+                  </Typography>
+                </ListItem>
+              </List>
+            </Typography>
+            <Typography>
+              Year 5 End Date: {viewingApplicationData?.yEnd}
+            </Typography>
+            <Typography>
+              Intent Letter :
+              <a href={SERVER_URL.slice(0, -1) + viewingApplicationData?.intentLetter}>
+                <Button variant='contained'>
+                  Download
+                </Button>
+              </a>
+            </Typography>
+          </Typography>
+        </Box>
+      </Modal>
+    )
+  }
+
   return (
     <>
+      <ApplicationPopUp />
       <TableContainer>
         <Table>
           <TableHead>
@@ -62,7 +182,10 @@ const PGPRs = () => {
                 <TableCell>{pgpr.groupedWith ? "Yes" : "No"}</TableCell>
                 <TableCell>
                   <ButtonGroup>
-                    <Button variant="contained" color="primary" size="small">View</Button>
+                    <Button variant="contained" color="primary" size="small" onClick={() => {
+                      setViewingApplication(pgpr.pgprApplicationId);
+                      openApplicationPopUp();
+                    }}>View</Button>
                   </ButtonGroup>
                 </TableCell>
                 <TableCell>{pgpr.statusOfPgpr}</TableCell>
