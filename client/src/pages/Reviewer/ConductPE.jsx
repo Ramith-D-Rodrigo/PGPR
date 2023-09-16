b81c070878dc9748a8514c320ec3d6d5ece48887
TharinduSampath2000 <stharindu444@gmail.com>
2023-09-17 03:47:12 +0530

    Finished Reviewer UI: Conduct PE

--- client/src/pages/Reviewer/ConductPE.jsx
+++ client/src/pages/Reviewer/ConductPE.jsx
@@ -10,16 +10,27 @@ import {
   TableRow,
   TableCell,
   Typography,
+  Divider,
+  Chip,
 } from "@mui/material";
 
 import DiscriptiveDiv from "../../components/DiscriptiveDiv";
-import { ConductPENavigation } from "../../navigations";
+import useSetUserNavigations from "../../hooks/useSetUserNavigations";
 
 const ConductPE = () => {
   const { pgprId } = useParams();
   const decodedPgprId = decodeURIComponent(pgprId);
 
-  <ConductPENavigation decodedPgprId={decodedPgprId} />;
+  useSetUserNavigations([
+    {
+      name: "PG Assignments",
+      link: "/PG_Assignments",
+    },
+    {
+      name: "PE",
+      link: `/PG_Assignments/Conduct_PE/${decodedPgprId}`,
+    },
+  ]);
 
   const headerInfo = [
     { label: "University:", value: "University of Colombo" },
@@ -89,71 +100,59 @@ const ConductPE = () => {
         </Grid>
       </DiscriptiveDiv>
 
-      <DiscriptiveDiv
-        description="Proper Evaluation"
-        width="100%"
-        height="auto"
-        backgroundColor="#D9D9D9"
-      >
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell style={{ backgroundColor: "#D8E6FC" }} align="left">
-                  <b>Name</b>
+      <Divider textAlign="left" sx={{ marginY: "1rem"}}>
+        <Chip label="Proper Evaluation" />
+      </Divider>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ backgroundColor: "#D8E6FC" }} align="left">
+                <b>Name</b>
+              </TableCell>
+              <TableCell style={{ backgroundColor: "#D8E6FC" }} align="center">
+                <b>Designation</b>
+              </TableCell>
+              <TableCell style={{ backgroundColor: "#D8E6FC" }} align="center">
+                <b>Status</b>
+              </TableCell>
+              <TableCell style={{ backgroundColor: "#D8E6FC" }} align="center">
+                <b>List of Criterian</b>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
                 </TableCell>
-                <TableCell
-                  style={{ backgroundColor: "#D8E6FC" }}
-                  align="center"
-                >
-                  <b>Designation</b>
-                </TableCell>
-                <TableCell
-                  style={{ backgroundColor: "#D8E6FC" }}
-                  align="center"
-                >
-                  <b>Status</b>
-                </TableCell>
-                <TableCell
-                  style={{ backgroundColor: "#D8E6FC" }}
-                  align="center"
-                >
-                  <b>List of Criterian</b>
+                <TableCell align="center">{row.designation}</TableCell>
+                <TableCell align="center">{row.status}</TableCell>
+                <TableCell align="center">
+                  <ul
+                    style={{
+                      listStyleType: "disc",
+                      textAlign: "left",
+                      paddingLeft: "20%",
+                    }}
+                  >
+                    {row.listOfCriteria.map((criteriaItem, index) => (
+                      <li key={index}>
+                        <Typography>{criteriaItem}</Typography>
+                      </li>
+                    ))}
+                  </ul>
                 </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row, index) => (
-                <TableRow
-                  key={index}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="center">{row.designation}</TableCell>
-                  <TableCell align="center">{row.status}</TableCell>
-                  <TableCell align="center">
-                    <ul
-                      style={{
-                        listStyleType: "disc",
-                        textAlign: "left",
-                        paddingLeft: "20%",
-                      }}
-                    >
-                      {row.listOfCriteria.map((criteriaItem, index) => (
-                        <li key={index}>
-                          <Typography>{criteriaItem}</Typography>
-                        </li>
-                      ))}
-                    </ul>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </DiscriptiveDiv>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
       <Grid
         container
