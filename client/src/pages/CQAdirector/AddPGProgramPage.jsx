399c10fe4f019931089fabae08109510f8d69bff
fiya99banu <fiyaza.fb@gmail.com>
2023-09-20 10:43:31 +0530

    new changes in addpgprogram

--- client/src/pages/CQAdirector/AddPGProgramPage.jsx
+++ client/src/pages/CQAdirector/AddPGProgramPage.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import Form from '../../components/Form';
-import MainContent from '../../components/MainContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import useSetUserNavigations from '../../hooks/useSetUserNavigations';
 
 
-
 const AddPGProgramPage = () => {
-  useSetUserNavigations(
-    [
-        {
-          name: "Add PG Program",
-          link: "/AddPGProgramPage"
-        },
-      
-    ]
-);
-  
+  useSetUserNavigations([
+    {
+      name: 'Add PG Program',
+      link: '/AddPGProgramPage',
+    },
+  ]);
+
   // Define your form submission logic here
-  const handleSubmit = (formValues) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const formValues = Object.fromEntries(formData.entries());
     // Handle form submission
     console.log(formValues);
   };
@@ -29,28 +29,122 @@ const AddPGProgramPage = () => {
   };
 
   return (
-   
-    
-      <Form
-        topic="Add PG Program"
-        fields={[
-          { name: 'university', label: 'University Name', type: 'text' },
-          { name: 'faculty', label: 'Faculty/University Name', type: 'text' },
-          { name: 'MgtUnit', label: 'PGPR Management Unit', type: 'text' },
-          { name: 'pgprname', label: 'PGPR Name', type: 'text' },
-          { name: 'startdate', label: 'Application Start Date', type: 'date'},
-          { name: 'subdate', label: 'Submission Date', type: 'date'},
-          { name: 'programCoordinator', label: 'Program Coordinator', type: 'text' },
-          { name: 'deanDirector', label: 'Dean/Director', type: 'text' },
-          { name: 'number', label: 'Phone', type: 'number' },
-          { name: 'slqfLevels', label: 'SLQF Level', type: 'text' },
-        ]}
-        cancelButtonText="Cancel"
-        submitButtonText="Add"
-        onCancel={handleCancel}
-        onSubmit={handleSubmit}
-      />
-    
+    <div className="max-w-6xl mx-auto p-6 bg-white rounded-md">
+      <h2 className="text-2xl font-bold mb-4 text-center">Add PG Program</h2>
+      <hr className="border-t-2 border-black my-4 opacity-50" />
+      <form onSubmit={handleSubmit}>
+
+
+
+<div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="University Name"
+    variant="outlined"
+    name="university"
+  />
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="Faculty/University Name"
+    variant="outlined"
+    name="faculty"
+  />
+</div>
+<div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="PGPR Management Unit"
+    variant="outlined"
+    name="MgtUnit"
+  />
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="PGPR Name"
+    variant="outlined"
+    name="pgprname"
+  />
+</div>
+<div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="Application Start Date"
+    variant="outlined"
+    name="startdate"
+    type="date"
+    InputLabelProps={{
+      shrink: true,
+    }}
+  />
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="Submission Date"
+    variant="outlined"
+    name="subdate"
+    type="date"
+    InputLabelProps={{
+      shrink: true,
+    }}
+  />
+</div>
+<div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="Program Coordinator"
+    variant="outlined"
+    name="programCoordinator"
+  />
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="Dean/Director"
+    variant="outlined"
+    name="deanDirector"
+  />
+</div>
+<div className="mb-4" style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="Phone"
+    variant="outlined"
+    name="number"
+    type="number"
+  />
+  <TextField
+    style={{ flexBasis: '48%', marginBottom: '1.5rem', height: '2rem' }}
+    fullWidth
+    label="SLQF Level"
+    variant="outlined"
+    name="slqfLevels"
+  />
+</div>
+
+
+
+
+        {/* Add similar TextField components for other form fields */}
+        <div className="flex justify-end">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleCancel}
+            style={{ marginRight: '16px' }}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" variant="contained" color="primary">
+            Add
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
