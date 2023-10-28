c58f35b2fd81761cc561eaa649c8479f92a14f65
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-10-28 21:36:14 +0530

    new mail handler for informing faculty related actions

--- server/app/Mail/InformFacultyActionToAuthorities.php
+++ server/app/Mail/InformFacultyActionToAuthorities.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -16,7 +17,14 @@ class InformFacultyActionToAuthorities extends Mailable
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(
+        public $user,
+        public $action,
+        public $facultyInfo,
+        public $university,
+        public $subject,
+        public $content
+    )
     {
         //
     }
@@ -44,7 +52,7 @@ class InformFacultyActionToAuthorities extends Mailable
     /**
      * Get the attachments for the message.
      *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     * @return array<int, Attachment>
      */
     public function attachments(): array
     {
