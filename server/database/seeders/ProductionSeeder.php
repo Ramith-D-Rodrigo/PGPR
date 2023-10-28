e07d53c407a0944707702173e112cd5d1f04e4ae
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-10-28 20:06:05 +0530

    Create ProductionSeeder.php

---
+++ server/database/seeders/ProductionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\QualityAssuranceCouncilOfficer;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //run criteria seeder
+        $this->call([CriteriaSeeder::class]);
+
+        //run standard seeder
+        $this->call([StandardSeeder::class]);
+
+
+        $officer = User::factory()->qac_officer()->hasQualityAssuranceCouncilOfficer()->create();
+        $director = QualityAssuranceCouncilOfficer::factory()->for(User::factory()->qac_director())->hasQualityAssuranceCouncilDirector()->create();
+    }
+}
