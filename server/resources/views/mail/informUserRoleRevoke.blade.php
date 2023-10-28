91f7ed6fea7ed437eaa10b653610f33e10505830
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-10-28 21:38:07 +0530

    updated the mail template for informing user role revocations

--- server/resources/views/mail/informUserRoleRevoke.blade.php
+++ server/resources/views/mail/informUserRoleRevoke.blade.php
@@ -1,12 +1,12 @@
 Sir/Madam,<br>
-{{$formerCQADirector['surname'] . " " . $formerCQADirector['initials']}},<br><br>
+{{$user['surname'] . " " . $user['initials']}},<br><br>
 
-Through this email, we are informing you that your role as the Center for Quality Assurance Director<br>
-has been removed. This will revoke your access privileges as the Center for Quality Assurance Director and<br>
-you will no longer be able to log in to the platform as the Center for Quality Assurance director or perform<br>
-any tasks related to the Center for Quality Assurance Director.<br>
+Through this email, we are informing you that your role as the {{$role}}<br>
+has been removed. This will revoke your access privileges as the {{$role}} and<br>
+you will no longer be able to log in to the platform as the {{$role}} or perform<br>
+any tasks related to the {{$role}}.<br>
 
-If this happened due to a mistake, please contact the Quality Assurance Council and request for amendments.<br>
+If this happened due to a mistake, please contact the authorities and request for amendments.<br>
 Thank you.<br>
 
 Regards,<br>
