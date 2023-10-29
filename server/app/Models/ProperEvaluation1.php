87c8752552d5ca54b7f76e0a4217af3a6fd0af5a
TharinduSampath2000 <stharindu444@gmail.com>
2023-10-29 18:01:31 +0530

    modified: Added fillable area

--- server/app/Models/ProperEvaluation1.php
+++ server/app/Models/ProperEvaluation1.php
@@ -10,6 +10,14 @@ class ProperEvaluation1 extends Model
     use HasFactory;
     protected $table = 'proper_evaluation_1s';
 
+    protected $fillable = [
+        'id',
+        'start_date',
+        'pe_1_meeting_date',
+        'end_date',
+        'remark'
+    ];
+
     // the proper evaluation 1 belongs to a ProperEvaluation
     public function properEvaluation()
     {
