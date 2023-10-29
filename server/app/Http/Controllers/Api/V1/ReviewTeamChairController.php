0b738c8b86d20e40c9cb075b30eeca41aa8e677a
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-10-29 23:10:06 +0530

    fixed error related to validate -> unsetting the 'pgpr_id'

--- server/app/Http/Controllers/Api/V1/ReviewTeamChairController.php
+++ server/app/Http/Controllers/Api/V1/ReviewTeamChairController.php
@@ -1066,6 +1066,7 @@ class ReviewTeamChairController extends Controller
         try {
             $validated = $request->validated();
             $properEvaluation1 = PostGraduateProgramReview::find($validated['pgpr_id'])->properEvaluations->properEvaluation1;
+            unset($validated['pgpr_id']);
             $properEvaluation1->update($validated);
             return response()->json(['message' => 'The update operation was successful']);
         } catch (Exception $exception) {
@@ -1086,6 +1087,7 @@ class ReviewTeamChairController extends Controller
         try {
             $validated = $request->validated();
             $properEvaluation2 = PostGraduateProgramReview::find($validated['pgpr_id'])->properEvaluations->properEvaluation2;
+            unset($validated['pgpr_id']);
             $properEvaluation2->update($validated);
             return response()->json(['message' => 'The update operation was successful']);
         } catch (Exception $exception) {
