c7cdadde1f0ca9a346f36a4e1d21b2ecf0beeb4e
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-10-29 16:24:46 +0530

    new mail class for handling notification actions related to pgpr applications

---
+++ server/resources/views/mail/informPostGraduateProgramReviewApplicationActionToAuthorities.blade.php
@@ -0,0 +1,30 @@
+@component('mail::message')
+    Sir/Madam,<br>
+    {{ $user['surname'] . ' ' . $user['initials'] }},<br><br>
+
+    We are emailing you to inform that,
+    @if ($action == 'CREATED')
+        new post graduate program application has been created.
+    @elseif ($action == 'UPDATED')
+        details associated with a postgraduate program review application has been updated.
+    @elseif ($action == 'SUMBITTED_FOR_CQA_DIR_APPROVAL')
+        a postgraduate program review application was submitted for your approval.
+    @elseif ($action == 'SUBMITTED_TO_QAC')
+        a postgraduate program application was submitted to the quality assurance council for consideration.
+    @elseif ($action == 'APPROVED_BY_QAC_OFFICER')
+        the postgraduate program review application was approved by the quality assurance coucil.
+    @endif
+
+    The following are some details related to the application.<br>
+
+    Postgraduate program: {{ $postGraduateProgram['title'] }}<br>
+    University: {{ $university['name'] }}<br>
+    Faculty: {{ $faculty['name'] }}<br>
+
+    You can visit our platform for more details.<br>
+    Thank you.<br><br>
+
+    Regards,<br>
+    Postgraduate Programme Review System,<br>
+    Quality Assurance Council
+@endcomponent
