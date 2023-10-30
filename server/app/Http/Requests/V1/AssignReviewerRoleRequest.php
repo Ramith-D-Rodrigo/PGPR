5c478d05bf149fe6e9def024c1597c37f1442404
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-10-30 23:58:23 +0530

    fix validation

--- server/app/Http/Requests/V1/AssignReviewerRoleRequest.php
+++ server/app/Http/Requests/V1/AssignReviewerRoleRequest.php
@@ -22,19 +22,29 @@ class AssignReviewerRoleRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'google_scholar_link' => ['required', 'string', 'url'],
-            'designation' => ['required', 'string'],
-            'qualification_1' => ['required', 'string', 'max:255'],
-            'qualification_1_slqf_level' => ['required', 'integer'],
-            'qualification_2' => ['required', 'string', 'max:255'],
-            'qualification_2_slqf_level' => ['required', 'integer'],
-            'qualification_3' => ['string', 'max:255', 'present', 'nullable'],
-            'qualification_3_slqf_level'=> ['integer', 'present', 'nullable'],
-            'qualification_4' => ['string', 'max:255', 'present', 'nullable'],
-            'qualification_4_slqf_level' => ['integer', 'present', 'nullable'],
-            'working_faculty' => ['required', 'integer', 'exists:faculties,id'],
-        ];
+        //get the universitySide from the called route
+        $universitySide = $this -> route('universitySide');
+
+        $roles = json_decode($universitySide -> user -> roles, true);
+
+        if(in_array('dean', $roles) || in_array('programme_coordinator', $roles)){
+            return [];
+        }
+        else{
+            return [
+                'google_scholar_link' => ['required', 'string', 'url'],
+                'designation' => ['required', 'string'],
+                'qualification_1' => ['required', 'string', 'max:255'],
+                'qualification_1_slqf_level' => ['required', 'integer'],
+                'qualification_2' => ['required', 'string', 'max:255'],
+                'qualification_2_slqf_level' => ['required', 'integer'],
+                'qualification_3' => ['string', 'max:255', 'present', 'nullable'],
+                'qualification_3_slqf_level'=> ['integer', 'present', 'nullable'],
+                'qualification_4' => ['string', 'max:255', 'present', 'nullable'],
+                'qualification_4_slqf_level' => ['integer', 'present', 'nullable'],
+                'working_faculty' => ['required', 'integer', 'exists:faculties,id'],
+            ];
+        }
     }
 
     public function prepareForValidation() {
