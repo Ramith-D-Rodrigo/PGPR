714c4be8ffe9304cc149fc6e2f299cf6ece6cda7
Bhathiya Vandebona <malingabhathiya@gmail.com>
2023-11-01 18:53:56 +0530

    removed redundant routes

---
+++ server/resources/views/mail/reviewerAcceptedPostrgaduateProgramReview.blade.php
@@ -0,0 +1,17 @@
+Sir/Madam,<br>
+{{$recipient['surname'] . " " . $recipient['initials']}},<br><br>
+
+The following user has rejected his/her assignment to the postgraduate program review, with the following comment.<br>
+Postgraduate Program: {{$post_grad_program['title']}}<br>
+Email: {{$reviewer['official_email']}}<br>
+Name: {{$reviewer['full_name']}}<br>
+Comment: {{$comment}}.<br>
+
+Please be kind enough to make follow-up calls to make sure that this was intentional.<br>
+If this happened due to a mistake you can make amends using the options provided on our website.<br><br>
+
+Thank you.<br><br>
+
+Regards,<br>
+Postgraduate Programme Review System,<br>
+Quality Assurance Council
