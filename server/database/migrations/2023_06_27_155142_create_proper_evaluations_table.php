2f1519c5d1bf6c05dde2fd9163216122df849b96
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-11-01 20:47:46 +0530

    error fixes

--- server/database/migrations/2023_06_27_155142_create_proper_evaluations_table.php
+++ server/database/migrations/2023_06_27_155142_create_proper_evaluations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
           $table->id();
           $table->foreignId('pgpr_id');
           $table->date('start_date');
-          $table->date('end_date');
+          $table->date('end_date')->nullable();
           $table->enum("stage", ['1', '2', 'COMPLETED'])->default('1');
           $table->timestamps();
 
